1e36a8fd71be69c533c9bd792e50f12999f9ca80
Flint Coffie <coffieflint91@gmail.com>
2026-02-09 15:21:48 +0000

    utterances

--- helsinki-mooc/mooc-programming-26/part01-09_utterances/src/utterances.py
+++ helsinki-mooc/mooc-programming-26/part01-09_utterances/src/utterances.py
@@ -1,5 +1,5 @@
 # Fix the code
-part = input("The 1st part: ")
-part = input("The 1st part: ")
-part = input("The 1st part: ")
-print(part + part + part)
+part_1 = input("The 1st part: ")
+part_2 = input("The 2nd part: ")
+part_3 = input("The 3rd part: ")
+print(part_1 +"-"+ part_2 +"-"+ part_3 +"!")
