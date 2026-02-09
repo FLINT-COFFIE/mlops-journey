6583a62fc44ddb250c0a4281b8812bc6960910a8
Flint Coffie <coffieflint91@gmail.com>
2026-02-09 15:07:42 +0000

    name and address

--- helsinki-mooc/mooc-programming-26/part01-08_name_and_address/src/name_and_address.py
+++ helsinki-mooc/mooc-programming-26/part01-08_name_and_address/src/name_and_address.py
@@ -1,1 +1,11 @@
 # Write your solution here
+#inputs
+first_name = input("Given name: ")
+second_name = input("Family name: ")
+address = input("Street address: ")
+postal_code = input("City and postal code: ")
+
+#output
+print(first_name+" "+second_name)
+print(address)
+print(postal_code)
