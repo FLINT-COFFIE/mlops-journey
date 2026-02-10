64b8666b718bff63cc215370da6b8b1edfaac9d6
Flint Coffie <coffieflint91@gmail.com>
2026-02-10 15:08:32 +0000

    seconds in given days

--- helsinki-mooc/mooc-programming-26/part01-15_seconds_in_a_day/src/seconds_in_a_day.py
+++ helsinki-mooc/mooc-programming-26/part01-15_seconds_in_a_day/src/seconds_in_a_day.py
@@ -1,1 +1,5 @@
 # Write your solution here
+days = int(input("Enter number of days: "))
+seconds = days * 24 * 60 * 60
+#there are 60 seconds in 1 min
+print(f"Seconds in that many days: {seconds}")
