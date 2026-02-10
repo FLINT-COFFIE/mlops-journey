30de34174325fc4dea6702eef2275adda065f67e
Flint Coffie <coffieflint91@gmail.com>
2026-02-10 15:39:35 +0000

    sum and mean

--- helsinki-mooc/mooc-programming-26/part01-18_sum_and_mean/src/sum_and_mean.py
+++ helsinki-mooc/mooc-programming-26/part01-18_sum_and_mean/src/sum_and_mean.py
@@ -1,1 +1,11 @@
 # Write your solution here
+#inputs
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter a second number: "))
+num3 = int(input("Enter a third number: "))
+num4 = int(input("Enter a fourth number: "))
+#outputs
+sum = num1+num2+num3+num4
+mean = sum / 4
+
+print(f"The sum of the numbers is {sum} and the mean is {mean}")
