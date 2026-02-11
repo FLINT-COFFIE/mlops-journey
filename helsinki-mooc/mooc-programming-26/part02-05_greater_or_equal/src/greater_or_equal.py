0139ff5bcb5e70eea4326ea128db4709c56c362b
Flint Coffie <coffieflint91@gmail.com>
2026-02-11 21:38:18 +0000

    greater or equal

--- helsinki-mooc/mooc-programming-26/part02-05_greater_or_equal/src/greater_or_equal.py
+++ helsinki-mooc/mooc-programming-26/part02-05_greater_or_equal/src/greater_or_equal.py
@@ -1,1 +1,10 @@
 # Write your solution here
+num1 = int(input("First Number: "))
+num2 = int(input("Second Number: "))
+
+if num1 > num2:
+    print(f"The greater number was: {num1}")
+elif num2 > num1:
+    print(f"The greater number was: {num2}")
+else:
+    print("The numbers are equal!")
