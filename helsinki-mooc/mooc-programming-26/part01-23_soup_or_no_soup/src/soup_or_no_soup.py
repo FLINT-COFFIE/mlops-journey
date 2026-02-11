dc1f2f90356cbdb0676b7867113652042bbb10ce
Flint Coffie <coffieflint91@gmail.com>
2026-02-11 10:21:39 +0000

    portions of soup

--- helsinki-mooc/mooc-programming-26/part01-23_soup_or_no_soup/src/soup_or_no_soup.py
+++ helsinki-mooc/mooc-programming-26/part01-23_soup_or_no_soup/src/soup_or_no_soup.py
@@ -1,1 +1,11 @@
 # Write your solution here
+name = input("Enter your name: ")
+
+#conditionals
+if name == "Jerry":
+    print("Next please!")
+    
+if name != "Jerry":
+    portion = int(input("How many portions: "))
+    price = 5.90 * portion
+    print(f"The total cost is {price} Next please!")
