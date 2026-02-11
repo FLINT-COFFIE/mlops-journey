a5960ca963d6eca4b678da91770911cb601315f7
Flint Coffie <coffieflint91@gmail.com>
2026-02-11 12:49:59 +0000

    daily_wages

--- helsinki-mooc/mooc-programming-26/part01-27_daily_wages/src/daily_wages.py
+++ helsinki-mooc/mooc-programming-26/part01-27_daily_wages/src/daily_wages.py
@@ -1,1 +1,12 @@
 # Write your solution here
+hourly_wage = float(input("Hourly wage: "))
+hours_worked = float(input("Hours worked: "))
+day = input("Day of the week: ")
+
+# Daily wages
+daily_wage = hourly_wage * hours_worked
+
+if day == "Sunday":
+    daily_wage *= 2
+
+print(f"Daily wages: {daily_wage} euros")
