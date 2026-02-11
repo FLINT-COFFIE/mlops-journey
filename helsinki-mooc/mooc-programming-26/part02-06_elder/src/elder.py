5c6b436bca94a4b64df87c2e9c843b6f337e20ca
Flint Coffie <coffieflint91@gmail.com>
2026-02-11 21:46:21 +0000

    elder between ages

--- helsinki-mooc/mooc-programming-26/part02-06_elder/src/elder.py
+++ helsinki-mooc/mooc-programming-26/part02-06_elder/src/elder.py
@@ -1,1 +1,15 @@
 # Write your solution here
+print("Person 1:")
+Person1 = input("Name: ")
+age1 = int(input("Age: "))
+
+print("Person 2:")
+Person2 = input("Name: ")
+age2 = int(input("Age: "))
+
+if age1 > age2:
+    print(f"The elder is {Person1}")
+elif age2 > age1:
+    print(f"The elder is {Person2}")
+else:
+    print(f"{Person1} and {Person2}  are the same age")
