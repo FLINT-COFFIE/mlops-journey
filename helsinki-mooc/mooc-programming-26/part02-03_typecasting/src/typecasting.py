173e049ccb1b428c3083b6b5a89938f5dc90c43c
Flint Coffie <coffieflint91@gmail.com>
2026-02-11 20:11:06 +0000

    typecasting

--- helsinki-mooc/mooc-programming-26/part02-03_typecasting/src/typecasting.py
+++ helsinki-mooc/mooc-programming-26/part02-03_typecasting/src/typecasting.py
@@ -1,1 +1,7 @@
 # Write your solution here
+num = float(input("Enter a number: "))
+integer = int(num)
+decimal = num - integer
+
+print(f"Interger part: {integer}")
+print(f"Decimal part: {decimal}")
