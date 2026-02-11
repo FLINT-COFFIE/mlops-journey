794c5721008887bea3d31989093a853a211e72dd
Flint Coffie <coffieflint91@gmail.com>
2026-02-11 14:33:58 +0000

    Quadratic Formula

--- helsinki-mooc/mooc-programming-26/part01-30_quadratic_formula/src/quadratic_formula.py
+++ helsinki-mooc/mooc-programming-26/part01-30_quadratic_formula/src/quadratic_formula.py
@@ -2,5 +2,16 @@
 # Let's take the square root of math-module in use
 from math import sqrt
 
+# defining variables
+a = float(input("a: "))
+b = float(input("b: "))
+c = float(input("c: "))
+
 # Note that the square root can also be calculated using power.
 # sqrt(9) is equivalent to 9 ** 0.5
+
+# roots
+x1 = (-b + sqrt(b * b - 4 * a * c)) / (2 * a)
+x2 = (-b - sqrt(b * b - 4 * a * c)) / (2 * a)
+# output
+print(f"The roots are {x1} and {x2}")
