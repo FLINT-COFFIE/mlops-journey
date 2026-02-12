214a282203638251bbef64b4b9758515d4525015
Flint Coffie <coffieflint91@gmail.com>
2026-02-12 21:31:29 +0000

    input validation

--- helsinki-mooc/mooc-programming-26/part02-16_input_validation/src/input_validation.py
+++ helsinki-mooc/mooc-programming-26/part02-16_input_validation/src/input_validation.py
@@ -1,2 +1,15 @@
 from math import sqrt
 # Write your solution here
+
+while True:
+    num = float(input("Enter a number: "))
+
+    if num < 0:
+        print("Invalid number")
+
+    elif num == 0:
+        print("Exiting...")
+        break
+
+    else:
+        print(sqrt(num))
