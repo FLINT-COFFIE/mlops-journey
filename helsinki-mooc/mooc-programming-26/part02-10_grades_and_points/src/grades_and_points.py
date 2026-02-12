d0903a7fcba5cbeb1bf5b0ab49d18c6cd256ed69
Flint Coffie <coffieflint91@gmail.com>
2026-02-12 13:49:47 +0000

    grades and points

--- helsinki-mooc/mooc-programming-26/part02-10_grades_and_points/src/grades_and_points.py
+++ helsinki-mooc/mooc-programming-26/part02-10_grades_and_points/src/grades_and_points.py
@@ -1,1 +1,27 @@
 # Write your solution here
+points = int(input("Enter amount of points received: "))
+
+# conditionals
+if points < 0:
+    print("impossible!")
+
+elif points < 50:
+    print("fail")
+
+elif points < 60:
+    print("Grade: 1")
+
+elif points < 70:
+    print("Grade: 2")
+
+elif points < 80:
+    print("Grade: 3")
+
+elif points < 90:
+    print("Grade: 4")
+
+elif points < 101:
+    print("Grade: 5")
+
+elif points > 100:
+    print("impossible!")
