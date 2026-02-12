852458631b55a2c8aada79891b15f5eca5f3c21a
Flint Coffie <coffieflint91@gmail.com>
2026-02-12 20:35:28 +0000

    middle element

--- helsinki-mooc/mooc-programming-26/part02-13_alphabetically_in_the_middle/src/alphabetically_in_the_middle.py
+++ helsinki-mooc/mooc-programming-26/part02-13_alphabetically_in_the_middle/src/alphabetically_in_the_middle.py
@@ -1,1 +1,23 @@
 # Write your solution here
+# letters
+first = input("1st letter: ")
+second = input("2nd letter: ")
+third = input("3rd letter: ")
+
+if first < second < third:
+    print(f"The letter in the middle is {second}")
+
+elif third < second < first:
+    print(f"The letter in the middle is {second}")
+
+elif second < first < third:
+    print(f"The letter in the middle is {first}")
+
+elif third < first < second:
+    print(f"The letter in the middle is {first}")
+
+elif second < third < first:
+    print(f"The letter in the middle is {third}")
+
+elif first < third < second:
+    print(f"The letter in the middle is {third}")
