2077d58d6449e81ae33a23b8d5dfd0928ed47479
Flint Coffie <coffieflint91@gmail.com>
2026-02-12 12:04:36 +0000

    nephews

--- helsinki-mooc/mooc-programming-26/part02-09_nephews/src/nephews.py
+++ helsinki-mooc/mooc-programming-26/part02-09_nephews/src/nephews.py
@@ -1,1 +1,12 @@
 # Write your solution here
+name = input("Enter your name: ")
+
+# conditionals
+if name == "Huey" or name == "Dewey" or name == "Louie":
+    print("I think you might be one of Donald Duck's nephews.")
+
+elif name == "Ferdie" or name == "Morty":
+    print("I think you might be one of Mickey Mouse's nephews.")
+
+else:
+    print("You're not a nephew of any character I know of.")
