e1eb000b6fa82d62f23ad90c396eaee5f730a6e2
Flint Coffie <coffieflint91@gmail.com>
2026-02-12 21:20:27 +0000

    intro to while loops

--- helsinki-mooc/mooc-programming-26/part02-15_shall_we_continue/src/shall_we_continue.py
+++ helsinki-mooc/mooc-programming-26/part02-15_shall_we_continue/src/shall_we_continue.py
@@ -1,1 +1,11 @@
 # Write your solution here
+print("hi")
+while True:
+    word = input("Shall we continue: ")
+
+    if word == "no":
+        print("okay then")
+        break
+
+    else:
+        print("hi")
