70ba7dcf166811c9614c191797bc166593733bcd
Flint Coffie <coffieflint91@gmail.com>
2026-02-13 10:29:46 +0000

    story

--- helsinki-mooc/mooc-programming-26/part02-21_story/src/story.py
+++ helsinki-mooc/mooc-programming-26/part02-21_story/src/story.py
@@ -1,1 +1,17 @@
 # Write your solution here
+codes = ""
+last = ""
+
+while True:
+    code = input("Please type in a word: ")
+
+    if code == "end":
+        print(codes)
+        break
+
+    if code == last:
+        print(codes)
+        break
+
+    codes += code + " "
+    last = code
