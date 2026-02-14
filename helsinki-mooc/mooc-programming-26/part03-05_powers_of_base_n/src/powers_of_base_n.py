838c6f1692df100414e9bd915ef64d354836657d
Flint Coffie <coffieflint91@gmail.com>
2026-02-14 18:03:51 +0000

    powers of base n

--- helsinki-mooc/mooc-programming-26/part03-05_powers_of_base_n/src/powers_of_base_n.py
+++ helsinki-mooc/mooc-programming-26/part03-05_powers_of_base_n/src/powers_of_base_n.py
@@ -1,1 +1,11 @@
 # Write your solution here
+lim = int(input("Upper limit: "))
+base = int(input("Base: "))
+
+power = 1
+start = 1
+
+while power <= lim:
+    print(power)
+    power = base**start
+    start += 1
