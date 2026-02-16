9089d386ab365caab62347f17efa00223e93784b
Flint Coffie <coffieflint91@gmail.com>
2026-02-16 15:19:10 +0000

    find all substrings

--- helsinki-mooc/mooc-programming-26/part03-21_find_all_substrings/src/find_all_substrings.py
+++ helsinki-mooc/mooc-programming-26/part03-21_find_all_substrings/src/find_all_substrings.py
@@ -1,1 +1,25 @@
 # Write your solution here
+word = input("Word: ")
+char = input("Enter character: ")
+# taking inputs
+
+"""while True:
+    output = word.find(char)
+
+    if output == -1:
+        break
+
+    elif len(word) >= output + 3:
+        print(word[output : output + 3])
+    word = word[output + 1 :]"""
+
+while True:
+    index = word.find(char)
+
+    if index == -1:
+        break
+
+    if len(word) >= index + 3:
+        print(word[index : index + 3])
+
+    word = word[index + 1 :]
