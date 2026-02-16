2d622ccc552aa4e419e67390a242d77fd71778fd
Flint Coffie <coffieflint91@gmail.com>
2026-02-16 11:41:21 +0000

    find substring

--- helsinki-mooc/mooc-programming-26/part03-20_find_first_substring/src/find_first_substring.py
+++ helsinki-mooc/mooc-programming-26/part03-20_find_first_substring/src/find_first_substring.py
@@ -1,1 +1,10 @@
-# Write your solution here
+# Write your solution
+word = input("Please type in a word: ")
+char = input("Please type in a character: ")
+
+start = word.find(char)
+
+output = word[start : start + 3]
+
+if len(output) > 2:
+    print(output)
