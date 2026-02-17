0ccb937cbb15b3f59035c53d5ff7dc1c59a8da85
Flint Coffie <coffieflint91@gmail.com>
2026-02-17 11:52:51 +0000

    multiplication

--- helsinki-mooc/mooc-programming-26/part03-23_multiplication/src/multiplication.py
+++ helsinki-mooc/mooc-programming-26/part03-23_multiplication/src/multiplication.py
@@ -1,1 +1,11 @@
 # Write your solution here
+num = int(input("Please type in a number: "))
+first = 0
+
+while first < num:
+    first += 1
+    second = 0
+
+    while second < num:
+        second += 1
+        print(f"{first} x {second} = {first * second}")
