850c53e794e245cab1ccb24d08cf5939c82e27a4
Flint Coffie <coffieflint91@gmail.com>
2026-02-17 15:28:32 +0000

    taking turns

--- helsinki-mooc/mooc-programming-26/part03-27_taking_turns/src/taking_turns.py
+++ helsinki-mooc/mooc-programming-26/part03-27_taking_turns/src/taking_turns.py
@@ -1,1 +1,16 @@
 # Write your solution here
+# take input
+num = int(input("Enter a number: "))
+
+first = 1
+second = num
+
+while first <= second:
+    if first == second:
+        print(first)
+        break
+    print(first)
+    print(second)
+
+    first += 1
+    second -= 1
