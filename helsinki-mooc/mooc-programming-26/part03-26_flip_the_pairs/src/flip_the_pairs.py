26277a3cab1e8090fb05dd9bf6b5ee573035a1b9
Flint Coffie <coffieflint91@gmail.com>
2026-02-17 14:43:16 +0000

    flipping pairs

--- helsinki-mooc/mooc-programming-26/part03-26_flip_the_pairs/src/flip_the_pairs.py
+++ helsinki-mooc/mooc-programming-26/part03-26_flip_the_pairs/src/flip_the_pairs.py
@@ -1,1 +1,15 @@
 # Write your solution here
+# take input
+num = int(input("Enter a number: "))
+# assign a variable ""
+count = 1
+
+while count <= num:
+    if count + 1 <= num:
+        print(count + 1)
+        print(count)
+
+    else:
+        print(count)
+
+    count += 2
