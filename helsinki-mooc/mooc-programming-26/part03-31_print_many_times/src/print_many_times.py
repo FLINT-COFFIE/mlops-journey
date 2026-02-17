bf8e36f9f9b6bdaff50b9a38b2ce43652c88647a
Flint Coffie <coffieflint91@gmail.com>
2026-02-17 20:25:08 +0000

    print multiple lines

--- helsinki-mooc/mooc-programming-26/part03-31_print_many_times/src/print_many_times.py
+++ helsinki-mooc/mooc-programming-26/part03-31_print_many_times/src/print_many_times.py
@@ -1,4 +1,10 @@
 # Write your solution here
+def print_many_times(word_to_print, lines):
+    num = 0
+    while num < lines:
+        print(word_to_print)
+        num += 1
+
 
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
