557e2cace541fd91fc6a3200692494acef07544c
Flint Coffie <coffieflint91@gmail.com>
2026-02-17 21:16:53 +0000

    chessboard

--- helsinki-mooc/mooc-programming-26/part03-33_chessboard/src/chessboard.py
+++ helsinki-mooc/mooc-programming-26/part03-33_chessboard/src/chessboard.py
@@ -1,4 +1,22 @@
 # Write your solution here
+def chessboard(limit):
+    row = 0
+
+    while row < limit:
+        line = ""
+        column = 0
+        while column < limit:
+            if (row + column) % 2 == 0:
+                line += "1"
+
+            else:
+                line += "0"
+
+            column += 1
+
+        print(line)
+        row += 1
+
 
 # Testing the function
 if __name__ == "__main__":
