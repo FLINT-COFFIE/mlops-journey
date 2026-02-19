f04a639c77c34dc10c61cf57b8372585d6739e87
Flint Coffie <coffieflint91@gmail.com>
2026-02-19 14:48:25 +0000

    square of hashes

--- helsinki-mooc/mooc-programming-26/part04-04_square_of_hashes/src/square_of_hashes.py
+++ helsinki-mooc/mooc-programming-26/part04-04_square_of_hashes/src/square_of_hashes.py
@@ -1,8 +1,24 @@
 # Copy here code of line function from previous exercise
+def line(num, char):
+    if len(char) < 1:
+        char = "*"
+
+    elif len(char) > 1:
+        char = char[0]
+    start = ""
+    while num > 0:
+        start += char
+        num -= 1
+    print(start)
+
 
 def square_of_hashes(size):
-    # You should call function line here with proper parameters
-    line(4, "#")
+    count = size
+    while count > 0:
+        # You should call function line here with proper parameters
+        line(size, "#")
+        count -= 1
+
 
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
