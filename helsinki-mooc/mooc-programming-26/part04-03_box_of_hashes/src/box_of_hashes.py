f97b40f25e9d87c08a15bc80353e877fb18656f8
Flint Coffie <coffieflint91@gmail.com>
2026-02-19 14:33:14 +0000

    functions within functions

--- helsinki-mooc/mooc-programming-26/part04-03_box_of_hashes/src/box_of_hashes.py
+++ helsinki-mooc/mooc-programming-26/part04-03_box_of_hashes/src/box_of_hashes.py
@@ -1,8 +1,25 @@
 # Copy here code of line function from previous exercise
+# Write your solution here
+def line(num, char):
+    if len(char) < 1:
+        char = "*"
+
+    elif len(char) > 1:
+        char = char[0]
+    start = ""
+    while num > 0:
+        start += char
+        num -= 1
+    print(start)
+
 
 def box_of_hashes(height):
-    # You should call function line here with proper parameters
-    line(10, "#")
+    limit = height
+    while limit > 0:
+        # You should call function line here with proper parameters
+        line(10, "#")
+        limit -= 1
+
 
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
