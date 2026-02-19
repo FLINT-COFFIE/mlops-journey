3f08191cac90214d656008c5056bcb6b52d7d913
Flint Coffie <coffieflint91@gmail.com>
2026-02-19 10:48:17 +0000

    vscode setup

--- helsinki-mooc/mooc-programming-26/part04-01_hello_visual_studio_code/src/hello_visual_studio_code.py
+++ helsinki-mooc/mooc-programming-26/part04-01_hello_visual_studio_code/src/hello_visual_studio_code.py
@@ -1,1 +1,13 @@
 # Write your solution here
+while True:
+    editor = input("Editor: ")
+
+    if editor.lower() == "visual studio code":
+        print("an excellent choice!")
+        break
+
+    elif editor.lower() == "word":
+        print("awful")
+
+    else:
+        print("not good")
