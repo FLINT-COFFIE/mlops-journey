2276694a3bfb2a38090bb18fd9f53e26bdc23d20
Flint Coffie <coffieflint91@gmail.com>
2026-02-19 20:18:30 +0000

    a spruce

--- helsinki-mooc/mooc-programming-26/part04-08_spruce/src/spruce.py
+++ helsinki-mooc/mooc-programming-26/part04-08_spruce/src/spruce.py
@@ -1,4 +1,25 @@
 # Write your solution here
+
+
+def spruce(num):
+    # print the string
+    print("a spruce!")
+
+    # print row of all stars
+    row = 0
+
+    while row < num:
+        # max space
+        space = " " * (num - row - 1)
+        stars = "*" * (2 * row + 1)
+        print(f"{space}{stars}")
+        row += 1
+
+    print(" " * (num - 1) + "*")
+
+
 # You can test your function by calling it within the following block
-if __name__ == "__main__":
-    spruce(5)
+# if __name__ == "__main__":
+# spruce(5)
+
+spruce(5)
