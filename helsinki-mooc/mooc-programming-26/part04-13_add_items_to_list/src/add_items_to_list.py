5be89ac81d7044f7063770c05f7f0c67789dcc0f
Flint Coffie <coffieflint91@gmail.com>
2026-02-20 16:08:49 +0000

    adding items to list

--- helsinki-mooc/mooc-programming-26/part04-13_add_items_to_list/src/add_items_to_list.py
+++ helsinki-mooc/mooc-programming-26/part04-13_add_items_to_list/src/add_items_to_list.py
@@ -1,1 +1,12 @@
 # Write your solution here
+
+my_list = []
+
+items = int(input("How many items: "))
+
+count = 1
+while count <= items:
+    item = int(input(f"Item {count}: "))
+    my_list.append(item)
+    count += 1
+print(my_list)
