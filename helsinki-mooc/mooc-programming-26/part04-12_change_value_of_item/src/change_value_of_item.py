a7a388d88440c5988f67575de629ee055f77db13
Flint Coffie <coffieflint91@gmail.com>
2026-02-20 15:51:51 +0000

    changing value of an item

--- helsinki-mooc/mooc-programming-26/part04-12_change_value_of_item/src/change_value_of_item.py
+++ helsinki-mooc/mooc-programming-26/part04-12_change_value_of_item/src/change_value_of_item.py
@@ -1,1 +1,11 @@
 # Write your solution here
+my_list = [1, 2, 3, 4, 5]
+
+while True:
+    index = int(input("Index: "))
+    if index == -1 or 0 < index >= len(my_list):
+        break
+
+    replace = int(input("New_value: "))
+    my_list[index] = replace
+    print(my_list)
