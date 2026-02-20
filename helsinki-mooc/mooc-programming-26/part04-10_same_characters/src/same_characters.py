ea9cd0c20d39825d4400e21de21fe0bffa9268cb
Flint Coffie <coffieflint91@gmail.com>
2026-02-20 10:47:39 +0000

    same_characters

--- helsinki-mooc/mooc-programming-26/part04-10_same_characters/src/same_characters.py
+++ helsinki-mooc/mooc-programming-26/part04-10_same_characters/src/same_characters.py
@@ -1,4 +1,12 @@
 # Write your solution here
+
+
+def same_chars(word, int1, int2):
+    if int1 >= len(word) or int2 >= len(word) or int1 < 0 or int2 < 0:
+        return False
+    return word[int1] == word[int2]
+
+
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
-    print(same_chars("coder", 1, 2))
+    print(same_chars("aaaa", 1, 2))
