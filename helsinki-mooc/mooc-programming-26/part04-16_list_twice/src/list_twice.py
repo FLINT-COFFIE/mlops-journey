e7e45ceb14050bb163446be96a4f0f4c80a373ea
Flint Coffie <coffieflint91@gmail.com>
2026-02-21 12:57:07 +0000

    listed items in order and how they were inputted

--- helsinki-mooc/mooc-programming-26/part04-16_list_twice/src/list_twice.py
+++ helsinki-mooc/mooc-programming-26/part04-16_list_twice/src/list_twice.py
@@ -1,1 +1,12 @@
 # Write your solution here
+# define list here so it doesn't reset
+my_list = []
+while True:
+    item = int(input("New item: "))
+    if item == 0:
+        print("Bye!")
+        break
+
+    my_list.append(item)
+    print(f"The list now: {my_list}")
+    print(f"The list in order: {sorted(my_list)}")
