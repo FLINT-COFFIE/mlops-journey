e56117239b0edc37e1851715d9ed8e4f3ae433bc
Flint Coffie <coffieflint91@gmail.com>
2026-02-21 22:13:07 +0000

    Anagrams

--- helsinki-mooc/mooc-programming-26/part04-23_anagrams/src/anagrams.py
+++ helsinki-mooc/mooc-programming-26/part04-23_anagrams/src/anagrams.py
@@ -1,1 +1,12 @@
 # Write your solution here
+def anagrams(str1, str2):
+    list1 = sorted(str1.lower())
+    list2 = sorted(str2.lower())
+
+    if list1 == list2:
+        return True
+    return False
+
+
+if __name__ == "__main__":
+    anagrams(str1, str2)
