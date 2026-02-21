4dac8dc3351be83e5281b0e42c9e7942dff30f7a
Flint Coffie <coffieflint91@gmail.com>
2026-02-21 22:44:52 +0000

    palindromes

--- helsinki-mooc/mooc-programming-26/part04-24_palindromes/src/palindromes.py
+++ helsinki-mooc/mooc-programming-26/part04-24_palindromes/src/palindromes.py
@@ -1,4 +1,24 @@
 # Write your solution here
+def palindromes(word):
+    list1 = []
+    list2 = []
+    for char in word:
+        list1.append(char)
+    list2 = list1[::-1]
+
+    if list2 == list1:
+        return True
+    return False
+
+
+while True:
+    word = input("Please type in a palindrome: ")
+    if palindromes(word):
+        print(f"{word} is a palindrome!")
+        break
+    else:
+        print("that wasn't a palindrome")
+
 # Note, that at this time the main program should not be written inside
 # if __name__ == "__main__":
 # block!
