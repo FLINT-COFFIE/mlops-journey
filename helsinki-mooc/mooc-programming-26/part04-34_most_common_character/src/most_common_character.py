e665d85f516d80edc05da64ed4981a929a66e2bb
Flint Coffie <coffieflint91@gmail.com>
2026-02-22 16:06:57 +0000

    most common char

--- helsinki-mooc/mooc-programming-26/part04-34_most_common_character/src/most_common_character.py
+++ helsinki-mooc/mooc-programming-26/part04-34_most_common_character/src/most_common_character.py
@@ -1,1 +1,17 @@
 # Write your solution here
+def most_common_character(word):
+    common = 0
+    freq = ""
+    for char in word:
+        count = word.count(char)
+        if count >= common:
+            common = count
+
+        if word.count(char) == common:
+            freq = char
+    return freq
+
+
+if __name__ == "__main__":
+    second_string = "exemplaryelementary"
+    print(most_common_character(second_string))
