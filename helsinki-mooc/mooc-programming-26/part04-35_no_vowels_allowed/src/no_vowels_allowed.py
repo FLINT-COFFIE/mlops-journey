7c2b2df7a0e5d27e063f215f664280c1c97bb9a7
Flint Coffie <coffieflint91@gmail.com>
2026-02-22 18:49:36 +0000

    no vowels allowed

--- helsinki-mooc/mooc-programming-26/part04-35_no_vowels_allowed/src/no_vowels_allowed.py
+++ helsinki-mooc/mooc-programming-26/part04-35_no_vowels_allowed/src/no_vowels_allowed.py
@@ -1,1 +1,14 @@
 # Write your solution here
+def no_vowels(word):
+    new_word = ""
+    vowels = ["a", "e", "i", "o", "u"]
+    for char in word:
+        if char.lower() in vowels:
+            continue
+        new_word += char
+    return new_word
+
+
+if __name__ == "__main__":
+    my_string = "this is an example"
+    print(no_vowels(my_string))
