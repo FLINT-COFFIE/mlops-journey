0a3e2b5c110f877f6bb9eb28a22b712e3352f76a
Flint Coffie <coffieflint91@gmail.com>
2026-02-22 14:11:35 +0000

    everything reversed

--- helsinki-mooc/mooc-programming-26/part04-33_everything_reversed/src/everything_reversed.py
+++ helsinki-mooc/mooc-programming-26/part04-33_everything_reversed/src/everything_reversed.py
@@ -1,1 +1,13 @@
 # Write your solution here
+def everything_reversed(list1):
+    inverse = list1[::-1]
+    for i in range(len(inverse)):
+        inverse[i] = inverse[i][::-1]
+
+    return inverse
+
+
+if __name__ == "__main__":
+    my_list = ["Hi", "there", "example", "one more"]
+    new_list = everything_reversed(my_list)
+    print(new_list)
