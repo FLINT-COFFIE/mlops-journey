1dd7252bfacab1e04ec8e55ce4b53fca18b20cbd
Flint Coffie <coffieflint91@gmail.com>
2026-02-22 11:04:03 +0000

    shortest in list

--- helsinki-mooc/mooc-programming-26/part04-30_shortest_in_list/src/shortest_in_list.py
+++ helsinki-mooc/mooc-programming-26/part04-30_shortest_in_list/src/shortest_in_list.py
@@ -1,1 +1,13 @@
 # Write your solution here
+def shortest(str_list):
+    shortest = str_list[0]
+    for word in str_list:
+        if len(word) < len(shortest):
+            shortest = word
+    return shortest
+
+
+if __name__ == "__main__":
+    my_list = ["first", "second", "fourth", "eleventh"]
+    result = shortest(my_list)
+    print(result)
