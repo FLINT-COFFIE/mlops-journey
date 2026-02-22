c028b531389e4b8162656e4ecb4c73756d7856a5
Flint Coffie <coffieflint91@gmail.com>
2026-02-22 08:37:01 +0000

    even numbers

--- helsinki-mooc/mooc-programming-26/part04-26_even_numbers/src/even_numbers.py
+++ helsinki-mooc/mooc-programming-26/part04-26_even_numbers/src/even_numbers.py
@@ -1,1 +1,14 @@
 # Write your solution here
+def even_numbers(list1):
+    even_num = []
+    for even in list1:
+        if even % 2 == 0:
+            even_num.append(even)
+    return even_num
+
+
+if __name__ == "__main__":
+    my_list = [1, 2, 3, 4, 5]
+    new_list = even_numbers(my_list)
+    print("original", my_list)
+    print("new", new_list)
