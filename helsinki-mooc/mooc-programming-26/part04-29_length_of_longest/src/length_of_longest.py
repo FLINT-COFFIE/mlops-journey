31415f8c3154956ace675cc593c96914d3d374a4
Flint Coffie <coffieflint91@gmail.com>
2026-02-22 10:59:38 +0000

    length of longest

--- helsinki-mooc/mooc-programming-26/part04-29_length_of_longest/src/length_of_longest.py
+++ helsinki-mooc/mooc-programming-26/part04-29_length_of_longest/src/length_of_longest.py
@@ -1,1 +1,13 @@
 # Write your solution here
+def length_of_longest(str_list):
+    longest = ""
+    for word in str_list:
+        if len(word) > len(longest):
+            longest = word
+    return len(longest)
+
+
+if __name__ == "__main__":
+    my_list = ["first", "second", "fourth", "eleventh"]
+    result = length_of_longest(my_list)
+    print(result)
