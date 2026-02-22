14b1dbbd786a2a318bb484d366a039f59e8cd866
Flint Coffie <coffieflint91@gmail.com>
2026-02-22 08:31:00 +0000

    sum of positives

--- helsinki-mooc/mooc-programming-26/part04-25_sum_of_positives/src/sum_of_positives.py
+++ helsinki-mooc/mooc-programming-26/part04-25_sum_of_positives/src/sum_of_positives.py
@@ -1,1 +1,11 @@
 # Write your solution here
+def sum_of_positives(list1):
+    pos = []
+    for i in list1:
+        if i > 0:
+            pos.append(i)
+    return sum(pos)
+
+
+if __name__ == "__main__":
+    print("The result is", sum_of_positives([1, -2, 3, -4, 5]))
