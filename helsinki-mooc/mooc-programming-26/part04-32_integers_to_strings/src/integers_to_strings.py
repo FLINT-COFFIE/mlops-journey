17926f4605ac1453540f4a5ee7d68df736976e7e
Flint Coffie <coffieflint91@gmail.com>
2026-02-22 13:12:20 +0000

    int to strings and f string formatting

--- helsinki-mooc/mooc-programming-26/part04-32_integers_to_strings/src/integers_to_strings.py
+++ helsinki-mooc/mooc-programming-26/part04-32_integers_to_strings/src/integers_to_strings.py
@@ -1,1 +1,12 @@
 # Write your solution here
+def formatted(float_list):
+    new = []
+    for dec in float_list:
+        new.append(f"{dec:.2f}")
+    return new
+
+
+if __name__ == "__main__":
+    my_list = [1.234, 0.3333, 0.11111, 3.446]
+    new_list = formatted(my_list)
+    print(new_list)
