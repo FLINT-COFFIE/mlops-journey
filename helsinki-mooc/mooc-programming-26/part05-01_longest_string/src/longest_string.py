5fc24f102d2e46d5656ff4a91ab90ab10caa25d4
Flint Coffie <coffieflint91@gmail.com>
2026-02-25 14:45:51 +0000

    longest string

--- helsinki-mooc/mooc-programming-26/part05-01_longest_string/src/longest_string.py
+++ helsinki-mooc/mooc-programming-26/part05-01_longest_string/src/longest_string.py
@@ -1,1 +1,14 @@
 # Write your solution here
+def longest(strings: list):
+    long = 0
+    for word in strings:
+        if len(word) > long:
+            long = len(word)
+    for word in strings:
+        if len(word) == long:
+            return word
+
+
+if __name__ == "__main__":
+    strings = ["hi", "hiya", "hello", "howdydoody", "hi there"]
+    print(longest(strings))
