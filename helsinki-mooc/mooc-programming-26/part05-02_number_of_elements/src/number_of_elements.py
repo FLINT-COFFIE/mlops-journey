56037ee149ab2e0b4821d4011db38093227431da
Flint Coffie <coffieflint91@gmail.com>
2026-02-25 19:35:08 +0000

    number of elements

--- helsinki-mooc/mooc-programming-26/part05-02_number_of_elements/src/number_of_elements.py
+++ helsinki-mooc/mooc-programming-26/part05-02_number_of_elements/src/number_of_elements.py
@@ -1,9 +1,13 @@
 # Write your solution here
 def count_matching_elements(my_matrix: list, element: int):
+    # store count
     count = 0
+    # loop through row
     for row in my_matrix:
+        # loop through column
         for column in row:
             if column == element:
+                # increase count
                 count += 1
     return count
 
