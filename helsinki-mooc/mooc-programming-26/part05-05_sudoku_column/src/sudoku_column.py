7e1a01b082a3e9ccd076cc0c5102950c7217c636
Flint Coffie <coffieflint91@gmail.com>
2026-02-26 08:08:42 +0000

    sudoku column layout done.

--- helsinki-mooc/mooc-programming-26/part05-05_sudoku_column/src/sudoku_column.py
+++ helsinki-mooc/mooc-programming-26/part05-05_sudoku_column/src/sudoku_column.py
@@ -1,1 +1,26 @@
 # Write your solution here
+def column_correct(sudoku: list, column_no: int):
+    columns = []
+    # loop through row
+    for row in sudoku:
+        if row[column_no] == 0:
+            continue
+        elif row[column_no] > 0:
+            columns.append(row)
+    return columns
+
+
+sudoku = [
+    [9, 0, 0, 0, 8, 0, 3, 0, 0],
+    [2, 0, 0, 2, 5, 0, 7, 0, 0],
+    [0, 2, 0, 3, 0, 0, 0, 0, 4],
+    [2, 9, 4, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 7, 3, 0, 5, 6, 0],
+    [7, 0, 5, 0, 6, 0, 4, 0, 0],
+    [0, 0, 7, 8, 0, 3, 9, 0, 0],
+    [0, 0, 1, 0, 0, 0, 0, 0, 3],
+    [3, 0, 0, 0, 0, 0, 0, 0, 2],
+]
+
+print(column_correct(sudoku, 0))
+print(column_correct(sudoku, 1))
