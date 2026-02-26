286ec323d547011de07bf139a63aa8fc95155e9e
Flint Coffie <coffieflint91@gmail.com>
2026-02-26 08:21:07 +0000

    sudoku column completed

--- helsinki-mooc/mooc-programming-26/part05-05_sudoku_column/src/sudoku_column.py
+++ helsinki-mooc/mooc-programming-26/part05-05_sudoku_column/src/sudoku_column.py
@@ -6,21 +6,31 @@ def column_correct(sudoku: list, column_no: int):
         if row[column_no] == 0:
             continue
         elif row[column_no] > 0:
-            columns.append(row)
-    return columns
+            columns.append(row[column_no])
+
+    # check for counts
+    count = 1
+    for i in columns:
+        total = columns.count(i)
+
+        if total > count:
+            return False
+
+    return True
 
 
-sudoku = [
-    [9, 0, 0, 0, 8, 0, 3, 0, 0],
-    [2, 0, 0, 2, 5, 0, 7, 0, 0],
-    [0, 2, 0, 3, 0, 0, 0, 0, 4],
-    [2, 9, 4, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 7, 3, 0, 5, 6, 0],
-    [7, 0, 5, 0, 6, 0, 4, 0, 0],
-    [0, 0, 7, 8, 0, 3, 9, 0, 0],
-    [0, 0, 1, 0, 0, 0, 0, 0, 3],
-    [3, 0, 0, 0, 0, 0, 0, 0, 2],
-]
+if __name__ == "__main__":
+    sudoku = [
+        [9, 0, 0, 0, 8, 0, 3, 0, 0],
+        [2, 0, 0, 2, 5, 0, 7, 0, 0],
+        [0, 2, 0, 3, 0, 0, 0, 0, 4],
+        [2, 9, 4, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 7, 3, 0, 5, 6, 0],
+        [7, 0, 5, 0, 6, 0, 4, 0, 0],
+        [0, 0, 7, 8, 0, 3, 9, 0, 0],
+        [0, 0, 1, 0, 0, 0, 0, 0, 3],
+        [3, 0, 0, 0, 0, 0, 0, 0, 2],
+    ]
 
-print(column_correct(sudoku, 0))
-print(column_correct(sudoku, 1))
+    print(column_correct(sudoku, 0))
+    print(column_correct(sudoku, 1))
