4a8c8e4bbd84cb95d9fb2f552ff280509e373fc2
Flint Coffie <coffieflint91@gmail.com>
2026-02-28 21:48:31 +0000

    histogram of words

--- helsinki-mooc/mooc-programming-26/part05-16_histogram/src/histogram.py
+++ helsinki-mooc/mooc-programming-26/part05-16_histogram/src/histogram.py
@@ -1,1 +1,16 @@
 # Write your solution here
+def histogram(word: str):
+    collection = {}
+    for char in word:
+        if char not in collection:
+            collection[char] = "*"
+        else:
+            collection[char] += "*"
+
+    for key, value in collection.items():
+        print(f"{key} {value}")
+
+
+if __name__ == "__main__":
+    histogram("abba")
+    histogram("statistically")
