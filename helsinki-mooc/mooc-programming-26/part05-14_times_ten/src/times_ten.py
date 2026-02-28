5169cb8d93d13113fb2373b53238094e65ab19ff
Flint Coffie <coffieflint91@gmail.com>
2026-02-28 15:38:23 +0000

    times_ten dicttionaries

--- helsinki-mooc/mooc-programming-26/part05-14_times_ten/src/times_ten.py
+++ helsinki-mooc/mooc-programming-26/part05-14_times_ten/src/times_ten.py
@@ -1,1 +1,13 @@
 # Write your solution here
+def times_ten(start_index: int, end_index: int):
+    # use a for loop to append keys to the dict
+    by_ten = {}
+    for key in range(start_index, end_index + 1):
+        by_ten[key] = key * 10
+
+    return by_ten
+
+
+if __name__ == "__main__":
+    d = times_ten(3, 6)
+    print(d)
