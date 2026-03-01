488879d640c7ff9cb7c1232f1a3afa60ad1ca089
Flint Coffie <coffieflint91@gmail.com>
2026-03-01 14:09:50 +0000

    phonebook 1 pseudocoede

--- helsinki-mooc/mooc-programming-26/part05-17_phone_book_v1/src/phone_book_v1.py
+++ helsinki-mooc/mooc-programming-26/part05-17_phone_book_v1/src/phone_book_v1.py
@@ -1,1 +1,15 @@
 # Write your solution here
+# while loop to take commands
+# take input
+# command :int
+# name :
+# number:
+
+# If input is 3 break
+
+# create 2 functions for each
+# If input is 2 add
+
+# If input is 1, search
+
+# print("ok!")
