c9293cfba80fa712398e5f331888aea102b1a06a
Flint Coffie <coffieflint91@gmail.com>
2026-03-01 17:53:47 +0000

    invert dictionary without replacing done: seems helsinki want mutation with this one.

--- helsinki-mooc/mooc-programming-26/part05-19_invert_dictionary/src/invert_dictionary.py
+++ helsinki-mooc/mooc-programming-26/part05-19_invert_dictionary/src/invert_dictionary.py
@@ -1,1 +1,12 @@
 # Write your solution here
+def invert(dictionary: dict):
+    inverted = {}
+    for key, value in dictionary.items():
+        inverted[value] = key
+    return inverted
+
+
+s = {1: "first", 2: "second", 3: "third", 4: "fourth"}
+
+print(s)
+print(invert(s))
