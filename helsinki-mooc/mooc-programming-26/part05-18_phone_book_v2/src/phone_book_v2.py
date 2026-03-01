54a50f17b67d353ad9c4b6d45ab20c09977f652a
Flint Coffie <coffieflint91@gmail.com>
2026-03-01 15:42:26 +0000

    phonebook v2 completed

--- helsinki-mooc/mooc-programming-26/part05-18_phone_book_v2/src/phone_book_v2.py
+++ helsinki-mooc/mooc-programming-26/part05-18_phone_book_v2/src/phone_book_v2.py
@@ -1,1 +1,39 @@
 # Write your solution here
+# dictionary to hold all
+phonebook = {}
+
+# while loop to take commands
+while True:
+    # take input
+    command = int(input("command (1 search, 2 add, 3 quit): "))
+    # command :int
+    if command not in range(1, 4):  # <= 0 or command > 3:
+        continue
+
+    elif command == 3:
+        print("quitting...")
+        break
+
+    if command == 2:
+        # name :
+        name = input("name: ")
+
+        # number:
+        number = input("number: ")
+
+        if name not in phonebook:
+            phonebook[name] = []
+
+        phonebook[name].append(number)
+        print("ok!")
+
+    elif command == 1:
+        # name :
+        name = input("name: ")
+
+        if name in phonebook:
+            for digit in phonebook[name]:
+                print(digit)
+
+        else:
+            print("no number")
