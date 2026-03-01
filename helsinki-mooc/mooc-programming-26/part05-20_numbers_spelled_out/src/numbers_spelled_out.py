9797cdb67bffb431ec6670a2ee89dafb3af45baf
Flint Coffie <coffieflint91@gmail.com>
2026-03-01 21:05:28 +0000

    numbers spelled out baseline laid out

--- helsinki-mooc/mooc-programming-26/part05-20_numbers_spelled_out/src/numbers_spelled_out.py
+++ helsinki-mooc/mooc-programming-26/part05-20_numbers_spelled_out/src/numbers_spelled_out.py
@@ -1,1 +1,48 @@
 # Write your solution here
+def dict_of_numbers():
+    #hard code the building blocks
+    ones = {
+        1: "one",
+        2: "two",
+        3: "three",
+        4: "four",
+        5: "five",
+        6: "six",
+        7: "seven",
+        8: "eight",
+        9: "nine",
+        0: "zero",
+    }
+
+    teens = {
+        11:"eleven",
+        12:"twelve",
+        13:"thirteen",
+        14:"fourteen",
+        15:"fifteen",
+        16:"sixteen",
+        17:"seventeen",
+        18:"eithteen",
+        19:"nineteen"
+    }
+    
+    tens = {
+        10:"ten",
+        20:"twenty",
+        30:"thirty",
+        40:"forty",
+        50:"fifty",
+        60:"sixty",
+        70:"seventy",
+        80:"eighty",
+        90:"ninety"
+        
+    }
+    #empty dict
+    number = {}
+    #using a for loop to add the keys
+    for num in range(0,100):
+        #break num down and get the word in one variable
+        if
+        number[num] = word
+    
