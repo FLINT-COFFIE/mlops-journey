994bf515c94050925629274bd91b7cb8b3027588
Flint Coffie <coffieflint91@gmail.com>
2026-03-02 22:36:33 +0000

    student database part 2 done

--- helsinki-mooc/mooc-programming-26/part05-26_student_database/src/student_database.py
+++ helsinki-mooc/mooc-programming-26/part05-26_student_database/src/student_database.py
@@ -1,4 +1,5 @@
 # Write your solution here
+# PART 1
 def add_student(students: dict, student: str):
     students[student] = []
 
@@ -12,10 +13,28 @@ def print_student(students: dict, student: str):
             print(f"{student}:")
             print("no completed courses")
 
+        # PART 2
+
+        else:
+            print(f"{student}:")
+            print(f" {len(students[student])} completed courses:")
+
+            total = 0
+            for course in students[student]:
+                print(f"  {course[0]} {course[1]}")
+
+                total += course[1]
+            average_grade = total / len(students[student])
+
+            print(f" average grade {average_grade}")
+
+
+def add_course(students: dict, student: str, courses: tuple):
+    students[student].append(courses)
+
 
 students = {}
 add_student(students, "Peter")
-add_student(students, "Eliza")
+add_course(students, "Peter", ("Introduction to Programming", 3))
+add_course(students, "Peter", ("Advanced Course in Programming", 2))
 print_student(students, "Peter")
-print_student(students, "Eliza")
-print_student(students, "Jack")
