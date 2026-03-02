c1aa7328d96f64afb2194e1481b361a9107989fc
Flint Coffie <coffieflint91@gmail.com>
2026-03-02 10:19:25 +0000

    movie_database complete

--- helsinki-mooc/mooc-programming-26/part05-21_movie_database/src/movie_database.py
+++ helsinki-mooc/mooc-programming-26/part05-21_movie_database/src/movie_database.py
@@ -2,7 +2,6 @@
 def add_movie(database: list, name: str, director: str, year: int, runtime: int):
     movie = {"name": name, "director": director, "year": year, "runtime": runtime}
     database.append(movie)
-    return movie
 
 
 if __name__ == "__main__":
