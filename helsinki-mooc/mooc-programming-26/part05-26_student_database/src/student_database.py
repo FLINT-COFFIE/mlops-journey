96e82ce5f84c039e67db92c1ba5cade067c5feb7
Flint Coffie <coffieflint91@gmail.com>
2026-03-02 22:14:16 +0000

    student database part 1 done

--- helsinki-mooc/mooc-programming-26/part05-26_student_database/src/student_database.py
+++ helsinki-mooc/mooc-programming-26/part05-26_student_database/src/student_database.py
@@ -1,1 +1,21 @@
 # Write your solution here
+def add_student(students: dict, student: str):
+    students[student] = []
+
+
+def print_student(students: dict, student: str):
+    if student not in students:
+        print(f"{student}: no such person in the database")
+
+    else:
+        if len(students[student]) < 1:
+            print(f"{student}:")
+            print("no completed courses")
+
+
+students = {}
+add_student(students, "Peter")
+add_student(students, "Eliza")
+print_student(students, "Peter")
+print_student(students, "Eliza")
+print_student(students, "Jack")
