8258e05875c9c3e37608ac8706b96f4013cd73f3
Flint Coffie <coffieflint91@gmail.com>
2026-03-02 15:53:14 +0000

    oldest person debugging

--- helsinki-mooc/mooc-programming-26/part05-24_oldest_person/src/oldest_person.py
+++ helsinki-mooc/mooc-programming-26/part05-24_oldest_person/src/oldest_person.py
@@ -1,1 +1,19 @@
 # Write your solution here
+def oldest_person(people: list):
+    oldest = []
+    for person in people:
+        oldest.append(person[1])
+    old = max(oldest)
+
+    for person in people:
+        if old in person:
+            return person
+
+
+p1 = ("Adam", 1977)
+p2 = ("Ellen", 1985)
+p3 = ("Mary", 1953)
+p4 = ("Ernest", 1997)
+people = [p1, p2, p3, p4]
+
+print(oldest_person(people))
