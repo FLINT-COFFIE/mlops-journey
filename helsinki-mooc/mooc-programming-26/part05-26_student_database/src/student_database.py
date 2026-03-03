5caac72164c680799a19e911a4cbe376153811e5
Flint Coffie <coffieflint91@gmail.com>
2026-03-03 10:42:17 +0000

    student database almost done and yet to test

--- helsinki-mooc/mooc-programming-26/part05-26_student_database/src/student_database.py
+++ helsinki-mooc/mooc-programming-26/part05-26_student_database/src/student_database.py
@@ -30,11 +30,55 @@ def print_student(students: dict, student: str):
 
 
 def add_course(students: dict, student: str, courses: tuple):
-    students[student].append(courses)
+    course_name, grade = courses
+    found = False
+    if grade > 0:
+        for i in range(len(students[student])):
+            if students[student][i][0] == course_name:
+                found = True
+                if grade > students[student][i][1]:
+                    students[student][i] = courses
+                break
+
+        if found == False:
+            students[student].append(courses)
+
+
+def summary(students: dict):
+    count = 0
+    highest = ()
+    # avg = total grade / len(courses)
+    best_avg = 0
+
+    for key, values in students.items():
+        if len(values) > count:
+            highest = len(values), key
+            count = len(values)
+
+        for student in students:
+            grade = 0
+
+            # name = ""
+            for i in range(len(values)):
+                grade += values[i][1]
+            avg = grade / len(values)
+
+            if avg > best_avg:
+                best_avg = avg
+                name += student
+
+    print(f"students {len(students)}")
+    print(f"most courses completed {highest[0]} {highest[1]}")
+    print(f"best average grade {best_avg} {student}")
+    print(students)
 
 
 students = {}
 add_student(students, "Peter")
-add_course(students, "Peter", ("Introduction to Programming", 3))
-add_course(students, "Peter", ("Advanced Course in Programming", 2))
-print_student(students, "Peter")
+add_student(students, "Eliza")
+add_course(students, "Peter", ("Data Structures and Algorithms", 1))
+add_course(students, "Peter", ("Introduction to Programming", 1))
+add_course(students, "Peter", ("Advanced Course in Programming", 1))
+add_course(students, "Eliza", ("Introduction to Programming", 5))
+add_course(students, "Eliza", ("Introduction to Computer Science", 4))
+summary(students)
