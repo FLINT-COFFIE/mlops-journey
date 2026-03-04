6aa08402937406b46408eecfad6e443c1a8583ce
Flint Coffie <coffieflint91@gmail.com>
2026-03-04 21:11:16 +0000

    was supposed to create a function

--- helsinki-mooc/mooc-programming-26/part06-02_fruit_market/src/fruit_market.py
+++ helsinki-mooc/mooc-programming-26/part06-02_fruit_market/src/fruit_market.py
@@ -1,10 +1,17 @@
-# write your solution here
+# write your solution here'
+
 # open file
+with open("fruits.csv") as f:
+    # create a dictonary
+    fruits = {}
+    # loop through
+    for line in f:
+        # replace newline with nothing
+        line = line.replace("\n", "")
+        parts = line.split(";")
 
-# create a dictonary
+        fruit = parts[0]
+        # append key values and float values
+        fruits[fruit] = parts[1:]
 
-# loop through
-
-# float values
-
-# append key values
+    print(fruits)
