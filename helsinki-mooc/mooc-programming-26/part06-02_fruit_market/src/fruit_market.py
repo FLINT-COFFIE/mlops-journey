3b53c488084e8bb23f1e9fe2a77161ca64c986f5
Flint Coffie <coffieflint91@gmail.com>
2026-03-04 21:17:36 +0000

    debugged fruit_market

--- helsinki-mooc/mooc-programming-26/part06-02_fruit_market/src/fruit_market.py
+++ helsinki-mooc/mooc-programming-26/part06-02_fruit_market/src/fruit_market.py
@@ -1,17 +1,21 @@
 # write your solution here'
+def read_fruits():
+    # open file
+    with open("fruits.csv") as f:
+        # create a dictonary
+        fruits = {}
+        # loop through
+        for line in f:
+            # replace newline with nothing
+            line = line.replace("\n", "")
+            parts = line.split(";")
 
-# open file
-with open("fruits.csv") as f:
-    # create a dictonary
-    fruits = {}
-    # loop through
-    for line in f:
-        # replace newline with nothing
-        line = line.replace("\n", "")
-        parts = line.split(";")
+            fruit = parts[0]
+            price = float(parts[1:][0])
+            # append key values and float values
+            fruits[fruit] = price
 
-        fruit = parts[0]
-        # append key values and float values
-        fruits[fruit] = parts[1:]
+        print(fruits)
 
-    print(fruits)
+
+read_fruits()
