95be1ff893305309522054ae1e44b027149c8db9
FLINT COFFIE <coffieflint91@gmail.com>
2026-03-06 23:40:01 +0000

    spellcheck completed

--- helsinki-mooc/mooc-programming-26/part06-07_spellchecker/src/spellchecker.py
+++ helsinki-mooc/mooc-programming-26/part06-07_spellchecker/src/spellchecker.py
@@ -1,1 +1,25 @@
 # write your solution here
+# initialise a list of correct words
+words = []
+# open the file
+with open("wordlist.txt") as newfile:
+    for word in newfile:
+        # strip and split
+        word = word.strip()
+        word = word.split(";")
+        # add it to a list
+        words.append(word[0])
+
+# take user input
+text = input("Write text: ")
+char = text.split(" ")
+# print(char)
+for string in char:
+    if string.lower() not in words:
+        print(f"*{string}*", end=" ")
+    else:
+        print(f"{string}", end=" ")
+# print(words)
+print()
+# return the string
+# use end = " " then add the reset
