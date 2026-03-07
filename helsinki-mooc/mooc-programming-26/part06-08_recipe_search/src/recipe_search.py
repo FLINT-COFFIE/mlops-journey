50692ccf410690bb6669a104fd02643538e0219e
FLINT COFFIE <coffieflint91@gmail.com>
2026-03-07 16:20:53 +0000

    recipe search read function debugged

--- helsinki-mooc/mooc-programming-26/part06-08_recipe_search/src/recipe_search.py
+++ helsinki-mooc/mooc-programming-26/part06-08_recipe_search/src/recipe_search.py
@@ -2,12 +2,30 @@
 
 # define read to open the file and extract info
 def read(filename: str):
+    # open the file
     with open(filename) as newfile:
+        # helper lists
         recipe_book = []
+        current_recipe = []
+        # looping through lines
         for line in newfile:
             line = line.strip()
-            recipe_book.append(line)
+            # grouping the data
+            if line == "":
+                if len(current_recipe) > 0:
+                    recipe_book.append(current_recipe)
+                # reset
+                current_recipe = []
+            else:
+                current_recipe.append(line)
+        if len(current_recipe) > 0:
+            recipe_book.append(current_recipe)
+
         return recipe_book
+
+
+filename = "recipes2.txt"
+print(read(filename))
 
 
 # def search_by_name(filename:str,word:str):
@@ -22,8 +40,10 @@ def search_by_name(filename: str, word: str):
         if word in char.lower():
             rec.append(char)
             recipies.append(rec)
+    food_name = []
     for food in recipies:
-        return recipies[food]
+        food_name.append(food[0])
+    # return i (for i in food_name:)
 
 
-print(search_by_name("recipes1.txt", "cake"))
+# print(search_by_name("recipes1.txt", "cake"))
