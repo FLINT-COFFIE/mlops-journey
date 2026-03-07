a659fc11bcc8838d73fc7c82152f7d93fff515fe
FLINT COFFIE <coffieflint91@gmail.com>
2026-03-07 20:22:14 +0000

    90% done

--- helsinki-mooc/mooc-programming-26/part06-08_recipe_search/src/recipe_search.py
+++ helsinki-mooc/mooc-programming-26/part06-08_recipe_search/src/recipe_search.py
@@ -58,8 +58,32 @@ def search_by_time(filename: str, prep_time: int):
     return output
 
 
+# search by ingredient
+def search_by_ingredient(filename: str, ingredient: str):
+    recipes = read(filename)
+    ingredients = []
+    output = []
+    names = []
+    for item in recipes:
+        # name = item[0]
+        time = item[1]
+        name = item[0]
+        ingredients.append(item[2:])
+        for prep in ingredients:
+            if ingredient in prep:
+                if name in names:
+                    continue
+                names.append(name)
+                output.append(f"{name}, preparation time {time} min")
+
+    return output
+
+
+search_by_ingredient("recipes1.txt", "milk")
+
 # search_by_time("recipes1.txt", 20)
 
 if __name__ == "__main__":
     search_by_name("recipes1.txt", "cake")
     search_by_time("recipes1.txt", 20)
+    search_by_ingredient("recipes1.txt", "eggs")
