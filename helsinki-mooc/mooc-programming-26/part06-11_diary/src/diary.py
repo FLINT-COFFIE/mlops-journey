ba5b19567c2b9569518d7a7341dbe00037f352ef
FLINT COFFIE <coffieflint91@gmail.com>
2026-03-08 15:28:34 +0000

    2 dairy functions completed

--- helsinki-mooc/mooc-programming-26/part06-11_diary/src/diary.py
+++ helsinki-mooc/mooc-programming-26/part06-11_diary/src/diary.py
@@ -1,1 +1,20 @@
 # Write your solution here
+while True:
+    # displaying requirements
+    print("1 - add an entry, 2 - read entries, 0 - quit")
+
+    # ask for function
+    function = int(input("Function: "))
+
+    # exit if zero
+    if function == 0:
+        print("Bye now!")
+        break
+
+    # adding entries
+    elif function == 1:
+        entry = input("Dairy entry: ")
+
+        # open and read the file
+        with open("dairy.txt", "a") as file:
+            file.write(entry + "\n")
