efc606ff95e836c55172ed707d4356be3c90c801
FLINT COFFIE <coffieflint91@gmail.com>
2026-03-08 12:15:16 +0000

    created the read function and the get_station_data function from the citybikes data

--- helsinki-mooc/mooc-programming-26/part06-09_city_bikes/src/city_bikes.py
+++ helsinki-mooc/mooc-programming-26/part06-09_city_bikes/src/city_bikes.py
@@ -1,2 +1,27 @@
 # tee ratkaisu tänne
 # Write your solution here
+# read the file
+def read(filename: str):
+    lines = []
+    with open(filename) as f:
+        for line in f:
+            line = line.strip()
+            line = line.split(";")
+            if line[0] == "Longitude":
+                continue
+            lines.append(line)
+    return lines
+
+
+def get_station_data(filename: str):
+    station_data = {}
+    file_data = read(filename)
+    for line in file_data:
+        station = line[3]
+        coordinate = (int(line[0]), int(line[1]))
+        station_data[station] = coordinate
+
+    return station_data
+
+
+print(get_station_data("stations1.csv"))
