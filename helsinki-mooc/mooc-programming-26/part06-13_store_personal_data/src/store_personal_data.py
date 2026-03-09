f1934788805a793ad8db956a1cf8736bd8464aeb
Flint Coffie <coffieflint91@gmail.com>
2026-03-09 14:28:13 +0000

    store personal data skeleton done

--- helsinki-mooc/mooc-programming-26/part06-13_store_personal_data/src/store_personal_data.py
+++ helsinki-mooc/mooc-programming-26/part06-13_store_personal_data/src/store_personal_data.py
@@ -1,1 +1,10 @@
 # Write your solution here
+def store_personal_data(person: tuple):
+    with open("people.csv", "a") as file:
+        file.write(f"{person[0]};{person[1]};{person[2]}\n")
+
+
+# ask for input
+people = input("input person: ")
+person = tuple(people.split(","))
+store_personal_data(person)
