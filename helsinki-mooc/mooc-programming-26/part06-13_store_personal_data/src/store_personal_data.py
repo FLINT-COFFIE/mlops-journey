be65cc0fa866255647d3582ff76b20231e4bcdf1
Flint Coffie <coffieflint91@gmail.com>
2026-03-09 14:42:42 +0000

    store_personal_data completed

--- helsinki-mooc/mooc-programming-26/part06-13_store_personal_data/src/store_personal_data.py
+++ helsinki-mooc/mooc-programming-26/part06-13_store_personal_data/src/store_personal_data.py
@@ -1,10 +1,12 @@
 # Write your solution here
 def store_personal_data(person: tuple):
+    name, age, height = person
     with open("people.csv", "a") as file:
-        file.write(f"{person[0]};{person[1]};{person[2]}\n")
+        file.write(f"{name};{age};{height}\n")
 
 
 # ask for input
-people = input("input person: ")
-person = tuple(people.split(","))
-store_personal_data(person)
+people = "(Paul Paulson; 37; 175.5)"
+# input("input person: ")
+person = people.strip("()")
+person = tuple(person.split(";"))
