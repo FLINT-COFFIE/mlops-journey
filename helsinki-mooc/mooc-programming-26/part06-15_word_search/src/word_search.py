4d78d8db5d8e6fd5c3ca5057a889c21299ea37c5
Flint Coffie <coffieflint91@gmail.com>
2026-03-10 15:31:01 +0000

    word_search first part done

--- helsinki-mooc/mooc-programming-26/part06-15_word_search/src/word_search.py
+++ helsinki-mooc/mooc-programming-26/part06-15_word_search/src/word_search.py
@@ -1,4 +1,11 @@
 # Write your solution here
 # make a dictionary with the search term as the key and list of words as values
-def find_words(search_term:str):
-    
+def find_words(search_term: str):
+    with open("words.txt") as f:
+        for line in f:
+            line = line.strip()
+            if search_term.lower() in line:
+                print(line)
+
+
+find_words("vokes")
