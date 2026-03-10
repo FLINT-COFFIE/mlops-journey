8cdbdd5cf7ddd6175aab134f79b40fe8e624186e
Flint Coffie <coffieflint91@gmail.com>
2026-03-10 20:14:04 +0000

    restructuring word search

--- helsinki-mooc/mooc-programming-26/part06-15_word_search/src/word_search.py
+++ helsinki-mooc/mooc-programming-26/part06-15_word_search/src/word_search.py
@@ -1,11 +1,16 @@
 # Write your solution here
 # make a dictionary with the search term as the key and list of words as values
 def find_words(search_term: str):
+    results = []
     with open("words.txt") as f:
         for line in f:
             line = line.strip()
-            if search_term.lower() in line:
-                print(line)
-
+            
+            
+                
+            #
+            elif search_term.lower() == line:
+                results.append(line)
+    return results
 
 find_words("vokes")
