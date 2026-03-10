f01dc08b273049c95da091ca9d1944a0609248a5
Flint Coffie <coffieflint91@gmail.com>
2026-03-10 20:39:47 +0000

    word search completed

--- helsinki-mooc/mooc-programming-26/part06-15_word_search/src/word_search.py
+++ helsinki-mooc/mooc-programming-26/part06-15_word_search/src/word_search.py
@@ -5,12 +5,35 @@ def find_words(search_term: str):
     with open("words.txt") as f:
         for line in f:
             line = line.strip()
-            
-            
-                
-            #
+            # if wildcards
+            # if searchterm startswith *
+            if search_term.startswith("*"):
+                word = search_term[1:]
+                if line.endswith(word):
+                    results.append(line)
+
+            # if searchterm endswith *
+            elif search_term.endswith("*"):
+                word = search_term[:-1]
+                if line.startswith(word):
+                    results.append(line)
+
+            # dots
+            elif "." in search_term:
+                if len(line) == len(search_term):
+                    match = True
+                    for i in range(len(search_term)):
+                        if search_term[i] != "." and line[i] != search_term[i]:
+                            match = False
+                            break
+                    if match:
+                        results.append(line)
+
+            # returning match if no wildcards
             elif search_term.lower() == line:
                 results.append(line)
     return results
 
-find_words("vokes")
+
+if __name__ == "__main__":
+    print(find_words("*vokes"))
