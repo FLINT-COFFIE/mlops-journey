a318fcdcfb19ae5bde631c129d2b27772fc02fde
Flint Coffie <coffieflint91@gmail.com>
2026-03-11 22:09:02 +0000

    read_input completed

--- helsinki-mooc/mooc-programming-26/part06-17_read_input/src/read_input.py
+++ helsinki-mooc/mooc-programming-26/part06-17_read_input/src/read_input.py
@@ -3,15 +3,18 @@ def read_input(text: str, bound1: int, bound2: int):
 
     while True:
         try:
-            number = int(input(text))  # (input("Enter a number: "))
+            number = input(text)  # (input("Enter a number: "))
+            number = int(number)
+
+            if bound1 <= number <= bound2:
+                return number
+            else:
+                print(f"You must type in an integer between {bound1} and {bound2}")
 
         except ValueError:
-            print(f"You must type in a number between {bound1} and {bound2}\n")
-
-        if bound1 < number < bound2:
-            return number
+            print(f"You must type in an integer between {bound1} and {bound2}")
 
 
 if __name__ == "__main__":
-    number = read_input("Please type in a number: ", 5, 10)
+    number = read_input("Give a number", 95, 105)
     print("You typed in:", number)
