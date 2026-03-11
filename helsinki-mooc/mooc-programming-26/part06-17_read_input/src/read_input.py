679dbc955c48a88beb90f4ab6c1c57801d11a66f
FLINT COFFIE <coffieflint91@gmail.com>
2026-03-11 21:37:30 +0000

    debugging read_input

--- helsinki-mooc/mooc-programming-26/part06-17_read_input/src/read_input.py
+++ helsinki-mooc/mooc-programming-26/part06-17_read_input/src/read_input.py
@@ -2,6 +2,16 @@
 def read_input(text: str, bound1: int, bound2: int):
 
     while True:
-        number = int(input("Enter a number: "))
+        try:
+            number = int(input(text))  # (input("Enter a number: "))
+
+        except ValueError:
+            print(f"You must type in a number between {bound1} and {bound2}\n")
+
         if bound1 < number < bound2:
             return number
+
+
+if __name__ == "__main__":
+    number = read_input("Please type in a number: ", 5, 10)
+    print("You typed in:", number)
