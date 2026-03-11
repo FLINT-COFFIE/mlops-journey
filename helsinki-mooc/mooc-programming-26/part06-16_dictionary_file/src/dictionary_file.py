82175f2eb4e046cfdf05f79169ef8c1f01fa4680
Flint Coffie <coffieflint91@gmail.com>
2026-03-11 10:06:34 +0000

    dictionary file skeleton

--- helsinki-mooc/mooc-programming-26/part06-16_dictionary_file/src/dictionary_file.py
+++ helsinki-mooc/mooc-programming-26/part06-16_dictionary_file/src/dictionary_file.py
@@ -1,1 +1,21 @@
 # Write your solution here
+print("1 - Add word, 2 - Search, 3 - Quit")
+
+while True:
+    # take input
+    function = int(input("Function: "))
+
+    # Quitting
+    if function == 3:
+        print("Bye!")
+        break
+
+    # adding
+    if function == 1:
+        # words
+        fin = input("The word in Finnish")
+        eng = input("The word in English")
+
+        # add and print dictionary entry added
+
+    # searching
