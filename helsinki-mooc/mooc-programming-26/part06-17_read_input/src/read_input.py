63b649c43fadcf78fc609b36c8477593dd3e3c57
FLINT COFFIE <coffieflint91@gmail.com>
2026-03-11 21:22:24 +0000

    read_input skeleton completed

--- helsinki-mooc/mooc-programming-26/part06-17_read_input/src/read_input.py
+++ helsinki-mooc/mooc-programming-26/part06-17_read_input/src/read_input.py
@@ -1,1 +1,7 @@
 # Write your solution here
+def read_input(text: str, bound1: int, bound2: int):
+
+    while True:
+        number = int(input("Enter a number: "))
+        if bound1 < number < bound2:
+            return number
