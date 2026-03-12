fb959414b3485c0f8da981050f8fae84af37f505
Flint Coffie <coffieflint91@gmail.com>
2026-03-12 22:16:19 +0000

    incorrect lottery numbers filter for keys laidout

--- helsinki-mooc/mooc-programming-26/part06-19_incorrect_lottery_numbers/src/incorrect_lottery_numbers.py
+++ helsinki-mooc/mooc-programming-26/part06-19_incorrect_lottery_numbers/src/incorrect_lottery_numbers.py
@@ -1,1 +1,40 @@
 # Write your solution here
+# read and return dictionary
+def read():
+    lottery = {}
+    with open("lottery_numbers.csv") as file:
+        for line in file:
+            line = line.strip()
+            parts = line.split(";")
+
+            week = parts[0]
+            lottery_numbers = parts[1]
+            lottery[week] = lottery_numbers
+    return lottery
+
+
+# filter the data
+def filter_incorrect(lottery: dict):
+
+    keys = []
+    values = []
+
+    correct = {}
+
+    for key, value in lottery.items():
+        keys.append(key)
+        values.append(value)
+
+    for key in keys:
+        parts = key.split(" ")
+        key_num = parts[1]
+
+        try:
+            key_num = int(key_num)
+            correct[key] = value
+        except ValueError:
+            continue
+    return correct
+
+
+print(filter_incorrect(read()))
