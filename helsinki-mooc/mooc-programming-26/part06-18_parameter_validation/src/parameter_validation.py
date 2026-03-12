621ca49b42cb704a222e4c745192e4a5e46fb2e0
Flint Coffie <coffieflint91@gmail.com>
2026-03-12 14:11:26 +0000

    parameter validation debugging

--- helsinki-mooc/mooc-programming-26/part06-18_parameter_validation/src/parameter_validation.py
+++ helsinki-mooc/mooc-programming-26/part06-18_parameter_validation/src/parameter_validation.py
@@ -1,17 +1,19 @@
 # Write your solution here
 def new_person(name: str, age: int):
-    try:
-        name = str(name)
-        age = int(age)
 
-    except:
+    if " " not in name:
         raise ValueError
 
-    if 2 <= len(name) <= 40 and 0 < age <= 150:
-        return name, age
+    if age > 150:
+        raise ValueError
+
+    if 2 <= len(name) <= 40:
+        if 0 < age <= 150:
+            return name, age
 
     else:
         raise ValueError
 
 
-print(new_person("Flint Coffie", 10))
+if __name__ == "__main__":
+    print(new_person("Flint Coffie", 10))
