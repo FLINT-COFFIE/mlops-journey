d87c00c7edce4a6c875e192c6b612e310ac5b6a7
Flint Coffie <coffieflint91@gmail.com>
2026-03-12 14:03:11 +0000

    parameter validation

--- helsinki-mooc/mooc-programming-26/part06-18_parameter_validation/src/parameter_validation.py
+++ helsinki-mooc/mooc-programming-26/part06-18_parameter_validation/src/parameter_validation.py
@@ -1,1 +1,17 @@
 # Write your solution here
+def new_person(name: str, age: int):
+    try:
+        name = str(name)
+        age = int(age)
+
+    except:
+        raise ValueError
+
+    if 2 <= len(name) <= 40 and 0 < age <= 150:
+        return name, age
+
+    else:
+        raise ValueError
+
+
+print(new_person("Flint Coffie", 10))
