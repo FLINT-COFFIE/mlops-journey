0d5eada9428aca79f3dd1ddf91853cbb5bda7cde
Flint Coffie <coffieflint91@gmail.com>
2026-03-16 15:08:52 +0000

    first part written, debugging in process

--- helsinki-mooc/mooc-programming-26/part07-02_special_characters/src/special_characters.py
+++ helsinki-mooc/mooc-programming-26/part07-02_special_characters/src/special_characters.py
@@ -1,2 +1,18 @@
 # Write your solution here
+import string
+
+
 # define the function
+def separate_characters(my_string: str):
+    ascii_chars = ""
+    punctuation = ""
+    other_chars = ""
+
+    for char in my_string:
+        if char.ascii_letters:
+            ascii_chars += char
+
+    return ascii_chars
+
+
+parts = separate_characters("Olé!!! Hey, are ümläüts wörking?")
