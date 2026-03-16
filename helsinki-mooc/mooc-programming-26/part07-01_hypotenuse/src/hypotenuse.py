fa26df32613ab77643fea9337294cfc524e5aa2a
Flint Coffie <coffieflint91@gmail.com>
2026-03-16 11:04:14 +0000

    completed hypoteneuse

--- helsinki-mooc/mooc-programming-26/part07-01_hypotenuse/src/hypotenuse.py
+++ helsinki-mooc/mooc-programming-26/part07-01_hypotenuse/src/hypotenuse.py
@@ -9,6 +9,7 @@ def hypotenuse(leg1: float, leg2: float):
     return final_leg
 
 
-print(hypotenuse(3, 4))  # 5.0
-print(hypotenuse(5, 12))  # 13.0
-print(hypotenuse(1, 1))  # 1.4142135623730951
+if __name__ == "__main__":
+    print(hypotenuse(3, 4))  # 5.0
+    print(hypotenuse(5, 12))  # 13.0
+    print(hypotenuse(1, 1))  # 1.4142135623730951
