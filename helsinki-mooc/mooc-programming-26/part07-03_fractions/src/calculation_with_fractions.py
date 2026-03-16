73b909c9fbaf62a63597056bcb6f8efefb9d472d
Flint Coffie <coffieflint91@gmail.com>
2026-03-16 15:56:37 +0000

    completed fractions

--- helsinki-mooc/mooc-programming-26/part07-03_fractions/src/calculation_with_fractions.py
+++ helsinki-mooc/mooc-programming-26/part07-03_fractions/src/calculation_with_fractions.py
@@ -1,1 +1,19 @@
 # Write your solution here
+# starting fractionate
+
+# importing fractions
+from fractions import Fraction
+
+
+# defining the function
+def fractionate(amount: int) -> Fraction:
+    pieces = []
+    frac = Fraction(1, amount)
+
+    for i in range(amount):
+        pieces.append(frac)
+    return pieces
+
+
+if __name__ == "__main__":
+    print(fractionate(5))
