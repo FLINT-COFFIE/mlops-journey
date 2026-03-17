9e0e5d2967dfbaf23cfe3b5e11246b6b918fcb6b
Flint Coffie <coffieflint91@gmail.com>
2026-03-17 14:07:16 +0000

    updated the completed lottery numbers

--- helsinki-mooc/mooc-programming-26/part07-04_lottery_numbers/src/lottery_numbers.py
+++ helsinki-mooc/mooc-programming-26/part07-04_lottery_numbers/src/lottery_numbers.py
@@ -6,7 +6,7 @@ from random import *
 
 
 def lottery_numbers(amount: int, lower: int, upper: int):
-    number_pool = list(range(lower, upper))
+    number_pool = list(range(lower, upper + 1))
     draw = sample(number_pool, amount)
     return sorted(draw)
 
