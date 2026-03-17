80a8c9bb6407387c836e600cf1f2c6f8ec6dec71
Flint Coffie <coffieflint91@gmail.com>
2026-03-17 16:18:31 +0000

    password generator part 2 slightly completed

--- helsinki-mooc/mooc-programming-26/part07-06_password_generator_part_2/src/password_generator_part_2.py
+++ helsinki-mooc/mooc-programming-26/part07-06_password_generator_part_2/src/password_generator_part_2.py
@@ -1,1 +1,30 @@
 # Write your solution here
+# importing functions
+import string
+import random
+
+
+# defining the function
+def generate_password(length: int, val: bool, val2: bool):
+    alphabet = string.ascii_lowercase
+    digits = string.digits
+    if val:
+        alphabet += string.digits
+
+    password_list = []
+    for i in range(length):
+        char = random.choice(alphabet)
+        password_list.append(char)
+    password = "".join(password_list)
+
+    if val:
+        for i in digits:
+            if i in password:
+                return password
+    return password
+
+
+if __name__ == "__main__":
+    # testing
+    for i in range(10):
+        print(generate_password(8, True, False))
