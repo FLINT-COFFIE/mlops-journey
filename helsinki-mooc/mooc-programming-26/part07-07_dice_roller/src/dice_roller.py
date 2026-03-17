c6f1b5ede766aaa5e3870f7e89a3d1451857931d
Flint Coffie <coffieflint91@gmail.com>
2026-03-17 21:31:22 +0000

    compiled dice _roller

--- helsinki-mooc/mooc-programming-26/part07-07_dice_roller/src/dice_roller.py
+++ helsinki-mooc/mooc-programming-26/part07-07_dice_roller/src/dice_roller.py
@@ -10,6 +10,7 @@ def roll(die: str):
     die_B = [2, 2, 2, 5, 5, 5]
     die_C = [1, 4, 4, 4, 4, 4]
 
+    # condition for each die
     if die == "A":
         result = random.choice(die_A)
 
@@ -22,6 +23,36 @@ def roll(die: str):
     return result
 
 
+# play die function
+def play(die1: str, die2: str, times: int):
+
+    # assign both results to an empty list
+    result_1 = []
+    result_2 = []
+
+    # get results and populate lists
+    for i in range(times):
+        result_1.append(roll(die1))
+        result_2.append(roll(die2))
+
+    # count for each
+    count_1 = 0
+    count_2 = 0
+    count_tie = 0
+
+    # compare_each
+    for i in range(times):
+        # three if statements
+        if result_1[i] > result_2[i]:
+            count_1 += 1
+        elif result_2[i] > result_1[i]:
+            count_2 += 1
+        elif result_1[i] == result_2[i]:
+            count_tie += 1
+
+    return count_1, count_2, count_tie
+
+
 # testing
-for i in range(20):
+for i in range(3):
     print(roll("A"), " ", end="")
