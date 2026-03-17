2461e54e86fd5be54aeb653639c63d5d914095cd
Flint Coffie <coffieflint91@gmail.com>
2026-03-17 15:37:13 +0000

    completed letter squared

--- helsinki-mooc/mooc-programming-26/part05-27_letter_square/src/letter_square.py
+++ helsinki-mooc/mooc-programming-26/part05-27_letter_square/src/letter_square.py
@@ -1,7 +1,7 @@
 # Write your solution here
 import string
 
-#grid size
+# grid size
 layers = int(input("Layers: "))
 alphabet = string.ascii_uppercase
 size = (2 * layers) - 1
@@ -9,16 +9,15 @@ size = (2 * layers) - 1
 for row in range(size):
     row_str = ""
     for column in range(size):
-    
-    dist_top = row
-    dist_left = column
-    dist_bottom = (size - 1)- row
-    dist_right = (size -1) - column
+        dist_top = row
+        dist_left = column
+        dist_bottom = (size - 1) - row
+        dist_right = (size - 1) - column
 
-    min_dist = min(dist_top,dist_bottom, dist_left, dist_right)
-    
-    letter_index = layers - 1 - m
-    
-    row_str += alphabet[letter_index]
-    
+        min_dist = min(dist_top, dist_bottom, dist_left, dist_right)
+
+        letter_index = layers - 1 - min_dist
+
+        row_str += alphabet[letter_index]
+
     print(row_str)
