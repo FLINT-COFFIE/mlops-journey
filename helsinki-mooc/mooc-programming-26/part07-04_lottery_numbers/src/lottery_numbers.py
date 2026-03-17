6c9824ea7d42bb2c543c2a127671c2011ee57ca7
Flint Coffie <coffieflint91@gmail.com>
2026-03-17 14:00:05 +0000

    testing the function for errors

--- helsinki-mooc/mooc-programming-26/part07-04_lottery_numbers/src/lottery_numbers.py
+++ helsinki-mooc/mooc-programming-26/part07-04_lottery_numbers/src/lottery_numbers.py
@@ -1,4 +1,15 @@
 # Write your solution here
+# importing random
+from random import *
+
 # defining the function
 
 
+def lottery_numbers(amount: int, lower: int, upper: int):
+    number_pool = list(range(lower, upper))
+    draw = sample(number_pool, amount)
+    return draw
+
+
+for number in lottery_numbers(7, 1, 40):
+    print(number)
