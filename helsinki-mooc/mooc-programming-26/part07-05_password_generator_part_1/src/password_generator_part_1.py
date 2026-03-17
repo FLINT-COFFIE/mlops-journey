bb860bc4b070e63534e3b2b9e50561540634fe09
Flint Coffie <coffieflint91@gmail.com>
2026-03-17 14:37:19 +0000

    started password generator part 1

--- helsinki-mooc/mooc-programming-26/part07-05_password_generator_part_1/src/password_generator_part_1.py
+++ helsinki-mooc/mooc-programming-26/part07-05_password_generator_part_1/src/password_generator_part_1.py
@@ -1,1 +1,12 @@
 # Write your solution here
+# importing functions
+from string import *
+from random import *
+
+# defining the function
+
+
+def password_gen(length: int):
+    alphabet = string.ascii_lowercase
+    password = sample(alphabet, length)
+    return password
