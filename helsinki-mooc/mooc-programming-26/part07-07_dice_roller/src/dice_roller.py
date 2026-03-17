c28fd52d5796a2501c7621c186e24b118a7f234e
Flint Coffie <coffieflint91@gmail.com>
2026-03-17 21:11:17 +0000

    started the die roller

--- helsinki-mooc/mooc-programming-26/part07-07_dice_roller/src/dice_roller.py
+++ helsinki-mooc/mooc-programming-26/part07-07_dice_roller/src/dice_roller.py
@@ -1,1 +1,27 @@
 # Write your solution here
+# import random in order to use choice
+import random
+
+
+# define the roll function
+def roll(die: str):
+    # assigning values to the dies
+    die_A = [3, 3, 3, 3, 3, 6]
+    die_B = [2, 2, 2, 5, 5, 5]
+    die_C = [1, 4, 4, 4, 4, 4]
+
+    if die == "A":
+        result = random.choice(die_A)
+
+    if die == "B":
+        result = random.choice(die_B)
+
+    if die == "C":
+        result = random.choice(die_C)
+
+    return result
+
+
+# testing
+for i in range(20):
+    print(roll("A"), " ", end="")
