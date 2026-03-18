0b926aec770556f8b32ce9f0b2c432215d8dd0f0
Flint Coffie <coffieflint91@gmail.com>
2026-03-18 08:21:04 +0000

    random words skeleton complete

--- helsinki-mooc/mooc-programming-26/part07-08_random_words/src/random_words.py
+++ helsinki-mooc/mooc-programming-26/part07-08_random_words/src/random_words.py
@@ -1,1 +1,16 @@
 # Write your solution here
+# imports
+
+
+# defining the function
+def words(n: int, beginning: str):
+    # store the words in a list
+    words = []
+    with open("words.txt") as word_file:
+        for line in word_file:
+            word = line.strip()
+
+            # condition to fill words
+            if word.startswith(beginning):
+                words.append(words)
+    return words
