aff8613e1dca5d216aa7d0c368904028e3033e18
Flint Coffie <coffieflint91@gmail.com>
2026-03-20 11:46:33 +0000

    code completed now testing and debugging

--- helsinki-mooc/mooc-programming-26/part07-10_valid_pic/src/valid_pic.py
+++ helsinki-mooc/mooc-programming-26/part07-10_valid_pic/src/valid_pic.py
@@ -7,22 +7,49 @@ from datetime import datetime
 def is_it_valid(pic: str):
     # only change this if invalid
     valid = True
+    century_markers = ["+", "-", "A"]
+    control_characters = "0123456789ABCDEFHJKLMNPRSTUVWXY"
 
-    # splitting into dates and other parts
-    day = int(pic[:2])
-    month = int(pic[2:4])
+    # early gate keeping
+    if len(pic) < 11 or pic[6] not in century_markers:
+        return False
 
-    # incomplete year
-    year = int(pic[4:6])
+    try:
+        # control marker check
+        control_num_str = f"{pic[:6]}{pic[7:10]}"
+        control_number = int(control_num_str)
 
-    if "-" in pic:
-        year += 1900
+        # control number check
+        control_index = control_number % 31
 
-    elif "+" in pic:
-        year += 1800
+        # control character
+        control_char = control_characters[control_index]
 
-    elif "A" in pic:
-        year += 2000
+        # control char gatekeeping
+        if pic[-1] != control_char:
+            return False
+
+        # splitting into dates and other parts
+        day = int(pic[:2])
+        month = int(pic[2:4])
+
+        # incomplete year
+        year = int(pic[4:6])
+
+        if "-" in pic:
+            year += 1900
+
+        elif "+" in pic:
+            year += 1800
+
+        elif "A" in pic:
+            year += 2000
+
+        # date of birth filter
+        date_of_birth = datetime(year, month, day)
+
+    except:
+        valid = False
 
     # return bool
     if valid:
@@ -30,3 +57,7 @@ def is_it_valid(pic: str):
 
     else:
         return False
+
+
+# testing
+print(is_it_valid("230827-906F"))
