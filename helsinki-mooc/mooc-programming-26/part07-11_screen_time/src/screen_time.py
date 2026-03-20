90b2066a77b1bff4847b26c9840b60870ca68b09
Flint Coffie <coffieflint91@gmail.com>
2026-03-20 16:34:33 +0000

    written initial info into file yet to append

--- helsinki-mooc/mooc-programming-26/part07-11_screen_time/src/screen_time.py
+++ helsinki-mooc/mooc-programming-26/part07-11_screen_time/src/screen_time.py
@@ -3,21 +3,39 @@
 from datetime import *
 
 # asking for inputs
-filename = input("Filename: ")
+# filename = input("Filename: ")
 start_date = input("Starting date: ")
-no_of_days = float(input("How many days: "))
+no_of_days = int(input("How many days: "))
+
+# calculated reformatting
+timedelta_days = timedelta(days=no_of_days)
 
 # reformatting data types
 # no_of_days = timedelta(days=no_of_days)
-start_date = start_date.strftime("%d.%m.%Y")
+start_date = datetime.strptime(start_date, "%d.%m.%Y")
+end_date = datetime.strptime((start_date + timedelta_days), "%d.%m.%Y")
+
+# difference
+difference = end_date - start_date
 
 # printing screen time input
 print("Please type in screen time in minutes on each day (TV computer mobile):\n")
 
+# writing to file
+with open("late_june.txt", "w") as file:
+    file.write(f"Time period: {start_date}-{end_date}\n")
+    file.write(f"Total minutes: {difference.min}\n")
+    file.write(f"How many days: {no_of_days}\n")
+    file.write(
+        "Please type in screen time in minutes on each day (TV computer mobile):\n"
+    )
+
+
 # looping through days
 for day in range(no_of_days):
     day = timedelta(days=day)
-    screen_time = input(f"Screen time {start_date + day}: ")
+    screen_time = input(f"Screen time {(start_date + day).strftime('%d.%m.%Y')}: ")
     print(screen_time)
 
-# print("Data stored in file late_june.txt")
+    # appending to file
+    # write("Data stored in file late_june.txt")
