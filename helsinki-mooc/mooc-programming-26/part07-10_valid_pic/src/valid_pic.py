9dd02be01cedee3f3672ad20cce21b1bf17dc419
Flint Coffie <coffieflint91@gmail.com>
2026-03-20 11:25:38 +0000

    added more depth

--- helsinki-mooc/mooc-programming-26/part07-10_valid_pic/src/valid_pic.py
+++ helsinki-mooc/mooc-programming-26/part07-10_valid_pic/src/valid_pic.py
@@ -8,7 +8,21 @@ def is_it_valid(pic: str):
     # only change this if invalid
     valid = True
 
-    #
+    # splitting into dates and other parts
+    day = int(pic[:2])
+    month = int(pic[2:4])
+
+    # incomplete year
+    year = int(pic[4:6])
+
+    if "-" in pic:
+        year += 1900
+
+    elif "+" in pic:
+        year += 1800
+
+    elif "A" in pic:
+        year += 2000
 
     # return bool
     if valid:
