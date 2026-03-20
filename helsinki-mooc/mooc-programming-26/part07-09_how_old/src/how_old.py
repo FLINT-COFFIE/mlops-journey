3f2a60517509e6a92715cdb75602546d22de49e7
Flint Coffie <coffieflint91@gmail.com>
2026-03-20 03:07:26 +0000

    used this year in the datetime example as well as remove the .days for testing purposes

--- helsinki-mooc/mooc-programming-26/part07-09_how_old/src/how_old.py
+++ helsinki-mooc/mooc-programming-26/part07-09_how_old/src/how_old.py
@@ -2,7 +2,7 @@
 from datetime import datetime
 
 time_now = datetime.now()
-midsummer = datetime(2021, 6, 26)
+midsummer = datetime(2026, 6, 26)
 
 difference = midsummer - time_now
 print("Midsummer is", difference, "days away")
