47a74ea69ef8568251d96ea2cfe2df090f6b817f
Flint Coffie <coffieflint91@gmail.com>
2026-03-20 21:03:23 +0000

    working with json files

--- helsinki-mooc/mooc-programming-26/part07-12_json_files/src/json_files.py
+++ helsinki-mooc/mooc-programming-26/part07-12_json_files/src/json_files.py
@@ -1,1 +1,9 @@
 # Write your solution here
+# importing json
+import json
+
+
+# writing the function
+def print_persons(filename: str):
+    with open(filename) as file:
+        profiles = file.read()
