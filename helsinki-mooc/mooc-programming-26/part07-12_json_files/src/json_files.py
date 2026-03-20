2aa9d36610e78f83956e7c21066020a4eb3f6d2c
Flint Coffie <coffieflint91@gmail.com>
2026-03-20 21:22:20 +0000

    debugging completed

--- helsinki-mooc/mooc-programming-26/part07-12_json_files/src/json_files.py
+++ helsinki-mooc/mooc-programming-26/part07-12_json_files/src/json_files.py
@@ -6,12 +6,16 @@ import json
 # writing the function
 def print_persons(filename: str):
     with open(filename) as file:
-        profiles = file.read()
+        data = file.read()
 
+    # loading the json into profiles
+    profiles = json.loads(data)
+
+    # looping through profiles
     for profile in profiles:
-        name = profile[name]
-        age = profile[age]
-        hobbies = profile[hobbies]
+        name = profile["name"]
+        age = profile["age"]
+        hobbies = ", ".join(profile["hobbies"])
 
         print(f"{name} {age} years ({hobbies})")
 
