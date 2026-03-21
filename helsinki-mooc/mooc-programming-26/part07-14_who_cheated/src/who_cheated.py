23009715ca84ea802e8c7c40376782bac75399f2
Flint Coffie <coffieflint91@gmail.com>
2026-03-21 21:38:07 +0000

    started who cheated

--- helsinki-mooc/mooc-programming-26/part07-14_who_cheated/src/who_cheated.py
+++ helsinki-mooc/mooc-programming-26/part07-14_who_cheated/src/who_cheated.py
@@ -1,1 +1,15 @@
 # Write your solution here
+# imports
+import datetime
+import csv
+
+
+# defining functions
+def cheaters():
+    with open("start_time.csv") as startfile:
+        for line in csv.reader(startfile, delimiter=";"):
+            print(line)
+
+
+# testing
+cheaters()
