0c653e99adacdaa95fccbde27cfb8307e2141b28
Flint Coffie <coffieflint91@gmail.com>
2026-03-22 13:35:59 +0000

    spellchecker

--- helsinki-mooc/mooc-programming-26/part07-16_spellchecker_2/src/spellchecker_2.py
+++ helsinki-mooc/mooc-programming-26/part07-16_spellchecker_2/src/spellchecker_2.py
@@ -1,1 +1,37 @@
 # Write your solution here
+#imports
+from difflib import get_close_matches
+
+##Part 1
+# initialise a list of correct words
+words = []
+# open the file
+with open("wordlist.txt") as newfile:
+    for word in newfile:
+        # strip and split
+        word = word.strip()
+        word = word.split(";")
+        # add it to a list
+        words.append(word[0])
+
+# take user input
+text = input("Write text: ")
+char = text.split(" ")
+# print(char)
+
+close_matches = {}
+wrong_words = []
+
+for string in char:
+    if string.lower() not in words:
+        wrong_words.append(string)
+        print(f"*{string}*", end=" ")
+        
+for wor
+# print(words)
+print()
+# return the string
+# use end = " " then add the reset
+
+
+##Part 2
