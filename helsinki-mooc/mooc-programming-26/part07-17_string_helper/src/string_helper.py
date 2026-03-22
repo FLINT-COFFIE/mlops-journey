87a6806849a3e4ab70b9129f6cb570979304bb38
Flint Coffie <coffieflint91@gmail.com>
2026-03-22 21:54:00 +0000

    made the first function

--- helsinki-mooc/mooc-programming-26/part07-17_string_helper/src/string_helper.py
+++ helsinki-mooc/mooc-programming-26/part07-17_string_helper/src/string_helper.py
@@ -1,2 +1,19 @@
 # Write your solution here
+# imports
+import string
 # writing my first module
+
+
+def change_case(orig_string: str):
+    # what it does
+    """creates and returns a new version of the parameter string. The lowercase letters in the original will be uppercase, and uppercase letters will be lowercase."""
+    replaced_str = ""
+    for char in orig_string:
+        if char.islower():
+            replaced_str += char.upper()
+        elif char.isupper():
+            replaced_str += char.lower()
+    return replaced_str
+
+
+change_case
