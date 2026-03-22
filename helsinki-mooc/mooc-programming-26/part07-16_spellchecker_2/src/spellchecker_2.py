5fe0f3fb674daca356c5ff16fc7fb1891cec0048
Flint Coffie <coffieflint91@gmail.com>
2026-03-22 13:50:24 +0000

    completed spell checker version 2

--- helsinki-mooc/mooc-programming-26/part07-16_spellchecker_2/src/spellchecker_2.py
+++ helsinki-mooc/mooc-programming-26/part07-16_spellchecker_2/src/spellchecker_2.py
@@ -1,5 +1,5 @@
 # Write your solution here
-#imports
+# imports
 from difflib import get_close_matches
 
 ##Part 1
@@ -10,9 +10,9 @@ with open("wordlist.txt") as newfile:
     for word in newfile:
         # strip and split
         word = word.strip()
-        word = word.split(";")
+        # word = word.split(";")
         # add it to a list
-        words.append(word[0])
+        words.append(word)
 
 # take user input
 text = input("Write text: ")
@@ -26,10 +26,18 @@ for string in char:
     if string.lower() not in words:
         wrong_words.append(string)
         print(f"*{string}*", end=" ")
-        
-for wor
-# print(words)
+    else:
+        print(f"{string}", end=" ")
+
+for word in wrong_words:
+    close_matches[word] = get_close_matches(word, words)
+
 print()
+print("suggestions:")
+
+# print(words)
+for key, values in close_matches.items():
+    print(f"{key}: {', '.join(values)}")
 # return the string
 # use end = " " then add the reset
 
