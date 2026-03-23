1b952cb6e7f4a937725ee901978fc271028b953d
Flint Coffie <coffieflint91@gmail.com>
2026-03-23 14:24:57 +0000

    debugging

--- helsinki-mooc/mooc-programming-26/part07-18_own_programming_language/src/own_language.py
+++ helsinki-mooc/mooc-programming-26/part07-18_own_programming_language/src/own_language.py
@@ -20,3 +20,61 @@ def run(program):
         if x in variables:
             return variables[x]
         return int(x)
+
+    # moving through
+    i = 0
+    while i < len(program):
+        parts = program[i].split()
+        command = parts[0]
+
+        # Calculations and output
+        if command == "PRINT":
+            results.append(get_value(parts[1]))
+        elif command == "MOV":
+            variables[parts[1]] = get_value(parts[2])
+        elif command == "ADD":
+            variables[parts[1]] += get_value(parts[2])
+        elif command == "SUB":
+            variables[parts[1]] -= get_value(parts[2])
+        elif command == "MUL":
+            variables[parts[1]] *= get_value(parts[2])
+
+        elif command == "JUMP":
+            i = labels[parts[1]]
+            continue
+
+        elif command == "IF":
+            value1 = get_value(parts[1])
+            operator = parts[2]
+            value2 = get_value(parts[3])
+            target = parts[5]
+
+        condition_met = False
+        if operator == "==":
+            condition_met = value1 == value2
+        elif operator == "!=":
+            condition_met = value1 != value2
+        elif operator == "<=":
+            condition_met = value1 <= value2
+        elif operator == ">=":
+            condition_met = value1 >= value2
+        elif operator == "<":
+            condition_met = value1 < value2
+        elif operator == ">":
+            condition_met = value1 > value2
+
+        if condition_met:
+            i = labels[target]
+            continue
+
+        elif command == "END":
+            break
+
+        i += 1
+
+    return results
+
+
+if __name__ == "__main__":
+    ##testing
+    run(program)
