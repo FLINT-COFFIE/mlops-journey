7c1d6095ae56e2c6c09f68c6b2da3a37db552054
Flint Coffie <coffieflint91@gmail.com>
2026-03-23 19:25:15 +0000

    added complexity analysis and renamed neetcode to neetcode 150

--- neetcode_150/arrays_and_hashing/contains_duplicate.py
+++ neetcode_150/arrays_and_hashing/contains_duplicate.py
@@ -1,4 +1,9 @@
+# Time complexity: O(n)
+
+# Space complexity: O(n)
+
+
 class Solution:
     def hasDuplicate(self, nums: List[int]) -> bool:
         unique = list(set(nums))
-        return len(unique) != len(nums) 
+        return len(unique) != len(nums)
