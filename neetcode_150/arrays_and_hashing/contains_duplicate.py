ce50886821d861c49286b0980c8c06f6b4dece5c
Flint Coffie <coffieflint91@gmail.com>
2026-03-23 19:27:01 +0000

    tweaked code to remove syntax errors

--- neetcode_150/arrays_and_hashing/contains_duplicate.py
+++ neetcode_150/arrays_and_hashing/contains_duplicate.py
@@ -4,6 +4,6 @@
 
 
 class Solution:
-    def hasDuplicate(self, nums: List[int]) -> bool:
+    def hasDuplicate(self, nums: list[int]) -> bool:
         unique = list(set(nums))
         return len(unique) != len(nums)
