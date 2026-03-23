8c89acfb038b2dfdbd0c529278876e572762c503
Flint Coffie <coffieflint91@gmail.com>
2026-03-23 20:12:25 +0000

    completed smallest average ready for testing

--- helsinki-mooc/mooc-programming-26/part08-01_smallest_average/src/smallest_average.py
+++ helsinki-mooc/mooc-programming-26/part08-01_smallest_average/src/smallest_average.py
@@ -1,1 +1,21 @@
 # Write your solution here
+# helper function
+def average(person: dict):
+    # helper variable
+    sum = 0
+    # loop through dict
+    for key, value in person.items():
+        if key == "name":
+            continue
+        sum += value
+    # defining average
+    average = sum / len(person)
+
+    # returning average
+    return average
+
+
+# defining function
+def smallest_average(person1: dict, person2: dict, person3: dict):
+    averages = [average(person1), average(person2), average(person3)]
+    return min(averages)
