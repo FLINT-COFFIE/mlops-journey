4c4904b7e5390a5cfce910878dd5b9153f29df35
Flint Coffie <coffieflint91@gmail.com>
2026-03-23 20:23:41 +0000

    row sums submitted

--- helsinki-mooc/mooc-programming-26/part08-02_row_sums/src/row_sums.py
+++ helsinki-mooc/mooc-programming-26/part08-02_row_sums/src/row_sums.py
@@ -1,4 +1,6 @@
 # Write your solution here
 def row_sums(my_matrix: list):
+    # looping through my_matrix
     for row in my_matrix:
+        # adding sum to each row
         row.append(sum(row))
