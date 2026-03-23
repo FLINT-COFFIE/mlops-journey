0a067ebd20bbb9640b47148f428eae5c217ff427
Flint Coffie <coffieflint91@gmail.com>
2026-03-23 09:18:06 +0000

    started language executor

--- helsinki-mooc/mooc-programming-26/part07-18_own_programming_language/src/own_language.py
+++ helsinki-mooc/mooc-programming-26/part07-18_own_programming_language/src/own_language.py
@@ -1,1 +1,22 @@
 # Write your solution here
+# imports
+import string
+
+
+# defining the function
+def run(program):
+    # A-Z
+    variables = {char: 0 for char in string.ascii_uppercase}
+    results = []
+
+    # whic lines contains commands with :
+    labels = {}
+    for index, line in enumerate(program):
+        if line.endswith(":"):
+            labels[line[:-1]] = index
+
+    # Function to check existing value or return an int
+    def get_value(x):
+        if x in variables:
+            return variables[x]
+        return int(x)
