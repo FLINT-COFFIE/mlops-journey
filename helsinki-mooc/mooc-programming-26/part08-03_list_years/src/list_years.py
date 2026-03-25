c92706bd128041e8220385c20ddec694d121da85
Flint Coffie <coffieflint91@gmail.com>
2026-03-25 21:34:54 +0000

    completed list_years

--- helsinki-mooc/mooc-programming-26/part08-03_list_years/src/list_years.py
+++ helsinki-mooc/mooc-programming-26/part08-03_list_years/src/list_years.py
@@ -1,3 +1,12 @@
 # Write your solution here
 # Remember the import statement
 # from datetime import date
+from datetime import date
+
+
+def list_years(dates: list):
+    years = []
+
+    for date in dates:
+        years.append(date.year)
+    return sorted(years)
