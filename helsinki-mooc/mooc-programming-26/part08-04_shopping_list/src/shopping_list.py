e82744a03bf00037e5f98bb30963918d9f61a9df
Flint Coffie <coffieflint91@gmail.com>
2026-03-26 13:55:27 +0000

    completed shopping list

--- helsinki-mooc/mooc-programming-26/part08-04_shopping_list/src/shopping_list.py
+++ helsinki-mooc/mooc-programming-26/part08-04_shopping_list/src/shopping_list.py
@@ -16,6 +16,22 @@ class ShoppingList:
     def amount(self, n: int):
         return self.products[n - 1][1]
 
+
 # -------------------------
 # Write your solution here:
 # -------------------------
+def total_units(my_list: ShoppingList):
+    count = 0
+    for i in range(1, my_list.number_of_items() + 1):
+        amount = my_list.amount(i)
+        count += amount
+    return count
+
+
+if __name__ == "__main__":
+    my_list = ShoppingList()
+    my_list.add("bananas", 10)
+    my_list.add("apples", 5)
+    my_list.add("pineapple", 1)
+
+    print(total_units(my_list))
