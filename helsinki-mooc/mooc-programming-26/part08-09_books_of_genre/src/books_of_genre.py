9769ae693b9ce777fed03875dadc8b2445065047
Flint Coffie <coffieflint91@gmail.com>
2026-03-28 20:50:13 +0000

    completed books of genre

--- helsinki-mooc/mooc-programming-26/part08-09_books_of_genre/src/books_of_genre.py
+++ helsinki-mooc/mooc-programming-26/part08-09_books_of_genre/src/books_of_genre.py
@@ -24,4 +24,17 @@ def books_of_genre(books: list, genre: str):
     for book in books:
         if book.genre == genre:
             genre_list.append(book)
-    return genre
+    return genre_list
+
+
+if __name__ == "__main__":
+    # testing
+    python = Book("Fluent Python", "Luciano Ramalho", "programming", 2015)
+    everest = Book("High Adventure", "Edmund Hillary", "autobiography", 1956)
+    norma = Book("Norma", "Sofi Oksanen", "crime", 2015)
+
+    books = [python, everest, norma, Book("The Snowman", "Jo Nesbø", "crime", 2007)]
+
+    print("Books in the crime genre:")
+    for book in books_of_genre(books, "crime"):
+        print(f"{book.author}: {book.name}")
