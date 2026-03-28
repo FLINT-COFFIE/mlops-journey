1bc1a587d9b9a0d879e601644cf2c32dbcdad971
Flint Coffie <coffieflint91@gmail.com>
2026-03-28 14:01:27 +0000

    defined the class Pet and yet to define the function

--- helsinki-mooc/mooc-programming-26/part08-07_pet/src/pet.py
+++ helsinki-mooc/mooc-programming-26/part08-07_pet/src/pet.py
@@ -1,1 +1,9 @@
 # Write your solution here:
+class Pet:
+    def __init__(self, name: str, species: str, year_of_birth: int):
+        self.name = name
+        self.species = species
+        self.year_of_birth = year_of_birth
+
+
+# defining the function
