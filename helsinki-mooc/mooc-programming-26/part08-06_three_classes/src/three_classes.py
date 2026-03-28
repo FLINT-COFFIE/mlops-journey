9586f3d1df50fda95758e79dc1dae32ebd331dd7
Flint Coffie <coffieflint91@gmail.com>
2026-03-28 13:16:51 +0000

    defined the seconed class

--- helsinki-mooc/mooc-programming-26/part08-06_three_classes/src/three_classes.py
+++ helsinki-mooc/mooc-programming-26/part08-06_three_classes/src/three_classes.py
@@ -4,3 +4,10 @@ class Checklist:
     def __init__(self, header: str, entries: list):
         self.header = header
         self.entries = entries
+class Customer:
+    def __init__(self, id: str, balance: float, discount: int):
+        self.id = id
+        self.balance = balance
+        self.discount = discount
+        
+class Cable:
