85e9499d1a6b22872043a0ce98eb7922a5e3f38e
Flint Coffie <coffieflint91@gmail.com>
2026-03-28 16:48:35 +0000

    defined the function. Ready for testing

--- helsinki-mooc/mooc-programming-26/part08-09_books_of_genre/src/books_of_genre.py
+++ helsinki-mooc/mooc-programming-26/part08-09_books_of_genre/src/books_of_genre.py
@@ -1,11 +1,12 @@
 # DO NOT CHANGE CLASS Book!
 # Write your solution after the class!
 
+
 class Book:
     def __init__(self, name: str, author: str, genre: str, year: int):
         self.name = name
         self.author = author
-        self.genre = genre 
+        self.genre = genre
         self.year = year
 
     ##STUB:# This enables easy printing of a Book object
@@ -16,3 +17,11 @@ class Book:
 # -----------------------------
 # Write your solution here
 # -----------------------------
+
+
+def books_of_genre(books: list, genre: str):
+    genre_list = []
+    for book in books:
+        if book.genre == genre:
+            genre_list.append(book)
+    return genre
