69e7d827140e9fb4c5a68f6139843b58a6d54660
Flint Coffie <coffieflint91@gmail.com>
2026-03-29 22:32:57 +0000

    completed clock

--- helsinki-mooc/mooc-programming-26/part08-14_clock/src/clock.py
+++ helsinki-mooc/mooc-programming-26/part08-14_clock/src/clock.py
@@ -8,15 +8,16 @@ class Clock:
     def set(self, hour, minute):
         self.hour = hour
         self.minute = minute
+        self.second = 0
 
     def tick(self):
-        self.seconds += 1
-        if self.seconds == 60:
-            self.seconds = 0
-            self.minutes += 1
+        self.second += 1
+        if self.second == 60:
+            self.second = 0
+            self.minute += 1
 
-        if self.minutes == 60:
-            self.minutes = 0
+        if self.minute == 60:
+            self.minute = 0
             self.hour += 1
 
         if self.hour == 24:
@@ -25,24 +26,25 @@ class Clock:
             self.second = 0
 
     def __str__(self):
-        return f"{self.hour:02d}:{self.minutes:02d}:{self.seconds:02d}"
+        return f"{self.hour:02d}:{self.minute:02d}:{self.second:02d}"
 
 
-# testing
-clock = Clock(23, 59, 55)
-print(clock)
-clock.tick()
-print(clock)
-clock.tick()
-print(clock)
-clock.tick()
-print(clock)
-clock.tick()
-print(clock)
-clock.tick()
-print(clock)
-clock.tick()
-print(clock)
+if __name__ == "__main__":
+    # testing
+    clock = Clock(23, 59, 55)
+    print(clock)
+    clock.tick()
+    print(clock)
+    clock.tick()
+    print(clock)
+    clock.tick()
+    print(clock)
+    clock.tick()
+    print(clock)
+    clock.tick()
+    print(clock)
+    clock.tick()
+    print(clock)
 
-clock.set(12, 5)
-print(clock)
+    clock.set(12, 5)
+    print(clock)
