5b2d696c46efc4ffacdcc50995c8df3a7f85d076
Flint Coffie <coffieflint91@gmail.com>
2026-03-29 16:51:00 +0000

    completed part 1

--- helsinki-mooc/mooc-programming-26/part08-12_number_stats/src/number_stats.py
+++ helsinki-mooc/mooc-programming-26/part08-12_number_stats/src/number_stats.py
@@ -1,10 +1,21 @@
 # Write your solution here!
-class  NumberStats:
+class NumberStats:
     def __init__(self):
         self.numbers = 0
+        self.count = 0
 
-    def add_number(self, number:int):
-        pass
+    def add_number(self, number: int):
+        self.numbers += number
+        self.count += 1
 
     def count_numbers(self):
-        pass
+        return self.count
+
+
+# testing
+stats = NumberStats()
+stats.add_number(3)
+stats.add_number(5)
+stats.add_number(1)
+stats.add_number(2)
+print("Numbers added:", stats.count_numbers())
