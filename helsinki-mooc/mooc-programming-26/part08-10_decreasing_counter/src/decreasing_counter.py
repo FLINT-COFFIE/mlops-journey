dd73f08360d12b0f1bc92f8fba0463d1c0832042
Flint Coffie <coffieflint91@gmail.com>
2026-03-29 14:02:59 +0000

    completed the final part

--- helsinki-mooc/mooc-programming-26/part08-10_decreasing_counter/src/decreasing_counter.py
+++ helsinki-mooc/mooc-programming-26/part08-10_decreasing_counter/src/decreasing_counter.py
@@ -2,15 +2,29 @@
 class DecreasingCounter:
     def __init__(self, initial_value: int):
         self.value = initial_value
+        self.original_value = initial_value
 
     def print_value(self):
         print("value:", self.value)
 
     def decrease(self):
-        if self.initial_value >= 1:
-            self.initial_value -= 1
-
-    def set_to_zero(self):
-        self.initial_value = 0
+        if self.value >= 1:
+            self.value -= 1
 
     # Write the rest of the methods here!
+    def set_to_zero(self):
+        self.value = 0
+
+    def reset_original_value(self):
+        self.value = self.original_value
+
+
+if __name__ == "__main__":
+    counter = DecreasingCounter(55)
+    counter.decrease()
+    counter.decrease()
+    counter.decrease()
+    counter.decrease()
+    counter.print_value()
+    counter.reset_original_value()
+    counter.print_value()
