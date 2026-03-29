dc4820f161bfe56695d59d6a752b6c63b0abbadb
Flint Coffie <coffieflint91@gmail.com>
2026-03-29 15:51:21 +0000

    defined class First and Last Name

--- helsinki-mooc/mooc-programming-26/part08-11_first_and_last_name/src/first_and_last_name.py
+++ helsinki-mooc/mooc-programming-26/part08-11_first_and_last_name/src/first_and_last_name.py
@@ -1,14 +1,17 @@
 # Write your solution here:
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    def return_first_name(self):
+        parts = self.name.split()
+        first_name = parts[0]
+        return first_name
+
+    def return_second_name(self):
+        parts = self.name.split()
+        second_name = parts[1]
+        return second_name
 
 
-
-
-
-
-
-
-
-
-
-
-
+# testing
