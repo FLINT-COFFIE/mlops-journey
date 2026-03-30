e09bdff459a1c11ac232f7b2d67c540e5ea09200
Flint Coffie <coffieflint91@gmail.com>
2026-03-30 21:50:22 +0000

    completed part 2

--- helsinki-mooc/mooc-programming-26/part08-15_lunchcard/src/lunchcard.py
+++ helsinki-mooc/mooc-programming-26/part08-15_lunchcard/src/lunchcard.py
@@ -1,3 +1,25 @@
 # Write your solution here:
 class LunchCard:
-    
+    def __init__(self, balance: float):
+        self.balance = balance
+
+    def eat_lunch(self):
+        self.balance -= 2.60
+
+    def eat_special(self):
+        self.balance -= 4.60
+
+    def __str__(self):
+        return f"The balance is {self.balance:.1f} euros."
+
+
+# testing
+card = LunchCard(50)
+print(card)
+
+card.eat_lunch()
+print(card)
+
+card.eat_special()
+card.eat_lunch()
+print(card)
