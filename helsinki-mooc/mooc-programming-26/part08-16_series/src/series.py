03ec7cbbdb857c0604be03e3efa302caa9303f31
Flint Coffie <coffieflint91@gmail.com>
2026-03-31 13:35:22 +0000

    part 1 of series done

--- helsinki-mooc/mooc-programming-26/part08-16_series/src/series.py
+++ helsinki-mooc/mooc-programming-26/part08-16_series/src/series.py
@@ -1,1 +1,14 @@
 # Write your solution here:
+class Series:
+    def __init__(self, name: str, seasons: int, genres: list):
+        self.name = name
+        self.seasons = seasons
+        self.genres = genres
+
+    def __str__(self):
+        return f"{self.name} ({self.seasons} seasons)\ngenres: {', '.join(self.genres)}\nno ratings"
+
+
+# testing
+dexter = Series("Dexter", 8, ["Crime", "Drama", "Mystery", "Thriller"])
+print(dexter)
