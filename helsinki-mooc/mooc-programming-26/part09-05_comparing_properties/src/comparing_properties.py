99c1f010c2451e0efa1cd23009683aa4cdec798c
Flint Coffie <coffieflint91@gmail.com>
2026-04-02 23:07:49 +0000

    completed comparing properties

--- helsinki-mooc/mooc-programming-26/part09-05_comparing_properties/src/comparing_properties.py
+++ helsinki-mooc/mooc-programming-26/part09-05_comparing_properties/src/comparing_properties.py
@@ -11,13 +11,25 @@ class RealProperty:
         return self.square_metres > compared_to.square_metres
 
     def price_difference(self, compared_to: "RealProperty"):
-        return abs(self.price_per_sqm - compared_to.price_per_sqm)
+        return abs(
+            self.price_per_sqm * self.square_metres
+            - compared_to.price_per_sqm * compared_to.square_metres
+        )
+
+    def more_expensive(self, compared_to: "RealProperty"):
+        if (
+            self.price_per_sqm * self.square_metres
+            > compared_to.price_per_sqm * compared_to.square_metres
+        ):
+            return True
+        return False
 
 
-# testing
-central_studio = RealProperty(1, 16, 5500)
-downtown_two_bedroom = RealProperty(2, 38, 4200)
-suburbs_three_bedroom = RealProperty(3, 78, 2500)
+if __name__ == "__main__":
+    # testing
+    central_studio = RealProperty(1, 16, 5500)
+    downtown_two_bedroom = RealProperty(2, 38, 4200)
+    suburbs_three_bedroom = RealProperty(3, 78, 2500)
 
-print(central_studio.price_difference(downtown_two_bedroom))
-print(suburbs_three_bedroom.price_difference(downtown_two_bedroom))
+    print(central_studio.more_expensive(downtown_two_bedroom))
+    print(suburbs_three_bedroom.more_expensive(downtown_two_bedroom))
