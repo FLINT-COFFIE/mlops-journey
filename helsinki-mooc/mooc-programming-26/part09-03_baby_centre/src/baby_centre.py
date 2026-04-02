13f451c6093218b80a1ae34a8c3a08c55f4a8f48
Flint Coffie <coffieflint91@gmail.com>
2026-04-02 15:58:04 +0000

    completed the baby centre

--- helsinki-mooc/mooc-programming-26/part09-03_baby_centre/src/baby_centre.py
+++ helsinki-mooc/mooc-programming-26/part09-03_baby_centre/src/baby_centre.py
@@ -27,22 +27,23 @@ class BabyCentre:
         return self.number_of_weigh_ins
 
 
-# testing
-baby_centre = BabyCentre()
+if __name__ == "__main__":
+    # testing
+    baby_centre = BabyCentre()
 
-eric = Person("Eric", 1, 110, 7)
-peter = Person("Peter", 33, 176, 85)
+    eric = Person("Eric", 1, 110, 7)
+    peter = Person("Peter", 33, 176, 85)
 
-print(f"Total number of weigh-ins is {baby_centre.weigh_ins()}")
+    print(f"Total number of weigh-ins is {baby_centre.weigh_ins()}")
 
-baby_centre.weigh(eric)
-baby_centre.weigh(eric)
+    baby_centre.weigh(eric)
+    baby_centre.weigh(eric)
 
-print(f"Total number of weigh-ins is {baby_centre.weigh_ins()}")
+    print(f"Total number of weigh-ins is {baby_centre.weigh_ins()}")
 
-baby_centre.weigh(eric)
-baby_centre.weigh(eric)
-baby_centre.weigh(eric)
-baby_centre.weigh(eric)
+    baby_centre.weigh(eric)
+    baby_centre.weigh(eric)
+    baby_centre.weigh(eric)
+    baby_centre.weigh(eric)
 
-print(f"Total number of weigh-ins is {baby_centre.weigh_ins()}")
+    print(f"Total number of weigh-ins is {baby_centre.weigh_ins()}")
