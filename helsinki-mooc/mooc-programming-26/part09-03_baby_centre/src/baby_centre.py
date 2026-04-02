5a7794e08a8ef6fba60efb3b3b86851c761ce810
Flint Coffie <coffieflint91@gmail.com>
2026-04-02 15:57:48 +0000

    completed the baby centre

--- helsinki-mooc/mooc-programming-26/part09-03_baby_centre/src/baby_centre.py
+++ helsinki-mooc/mooc-programming-26/part09-03_baby_centre/src/baby_centre.py
@@ -8,6 +8,7 @@ class Person:
         self.age = age
         self.height = height
         self.weight = weight
+        # self.weighins = 0
 
 
 class BabyCentre:
@@ -16,10 +17,14 @@ class BabyCentre:
 
     def weigh(self, person: Person):
         # return the weight of the person passed as an argument
+        self.number_of_weigh_ins += 1
         return person.weight
 
     def feed(self, person: Person):
         person.weight += 1
+
+    def weigh_ins(self):
+        return self.number_of_weigh_ins
 
 
 # testing
@@ -28,13 +33,16 @@ baby_centre = BabyCentre()
 eric = Person("Eric", 1, 110, 7)
 peter = Person("Peter", 33, 176, 85)
 
-print(f"{eric.name} weighs {baby_centre.weigh(eric)} kg")
-print(f"{peter.name} weighs {baby_centre.weigh(peter)} kg")
-print()
+print(f"Total number of weigh-ins is {baby_centre.weigh_ins()}")
 
-baby_centre.feed(eric)
-baby_centre.feed(eric)
-baby_centre.feed(eric)
+baby_centre.weigh(eric)
+baby_centre.weigh(eric)
 
-print(f"{eric.name} weighs {baby_centre.weigh(eric)} kg")
-print(f"{peter.name} weighs {baby_centre.weigh(peter)} kg")
+print(f"Total number of weigh-ins is {baby_centre.weigh_ins()}")
+
+baby_centre.weigh(eric)
+baby_centre.weigh(eric)
+baby_centre.weigh(eric)
+baby_centre.weigh(eric)
+
+print(f"Total number of weigh-ins is {baby_centre.weigh_ins()}")
