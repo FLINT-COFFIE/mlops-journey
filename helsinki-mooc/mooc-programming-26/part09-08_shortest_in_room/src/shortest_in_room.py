b4825da37c48e7c676a07084c068ab2c7cbe912d
Flint Coffie <coffieflint91@gmail.com>
2026-04-03 11:23:22 +0000

    completed part 1

--- helsinki-mooc/mooc-programming-26/part09-08_shortest_in_room/src/shortest_in_room.py
+++ helsinki-mooc/mooc-programming-26/part09-08_shortest_in_room/src/shortest_in_room.py
@@ -7,24 +7,30 @@ class Person:
     def __str__(self):
         return self.name
 
+
 class Room:
-    def __init__(self, people: list):
-        self.people = people
-        
+    def __init__(self):
+        self.people = []
+        self.sum_height = 0
+
     def add(self, person: Person):
-        self.people += person
-        
+        self.people.append(person)
+        self.sum_height += person.height
+
     def is_empty(self):
         if len(self.people) == 0:
             return True
         return False
-    
+
     def print_contents(self):
+        print(
+            f"There are {len(self.people)} persons in the room, and their combined height is {self.sum_height} cm"
+        )
         for person in self.people:
-            print person
-            
-            
-#testing
+            print(f"{person} ({person.height} cm)")
+
+
+# testing
 room = Room()
 print("Is the room empty?", room.is_empty())
 room.add(Person("Lea", 183))
