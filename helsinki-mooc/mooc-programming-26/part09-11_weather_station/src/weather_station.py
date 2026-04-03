7dadc3dfb0f3edb312be774a5ebe9e831f44f214
Flint Coffie <coffieflint91@gmail.com>
2026-04-03 20:55:18 +0000

    completed weather station and debugging

--- helsinki-mooc/mooc-programming-26/part09-11_weather_station/src/weather_station.py
+++ helsinki-mooc/mooc-programming-26/part09-11_weather_station/src/weather_station.py
@@ -1,3 +1,19 @@
 # WRITE YOUR SOLUTION HERE:
+class WeatherStation:
+    def __init__(self, city):
+        self.city = city
+        self.__observations = []
 
+    def add_observation(self, observation: str):
+        self.__observation.append(observation)
 
+    def latest_observation(self):
+        if len(self.__observations) == 0:
+            return ""
+        return self.__observations[-1]
+
+    def number_of_observations(self):
+        return len(self.__observations)
+
+    def __str__(self):
+        return f"{self.city}, {self.number_of_observations} observations"
