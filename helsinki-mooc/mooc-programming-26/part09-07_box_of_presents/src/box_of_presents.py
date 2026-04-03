ab39da84d86c192a8ecd75f3f0d0cc5204d5740b
Flint Coffie <coffieflint91@gmail.com>
2026-04-03 08:28:30 +0000

    completed present

--- helsinki-mooc/mooc-programming-26/part09-07_box_of_presents/src/box_of_presents.py
+++ helsinki-mooc/mooc-programming-26/part09-07_box_of_presents/src/box_of_presents.py
@@ -1,1 +1,27 @@
 # WRITE YOUR SOLUTION HERE:
+class Present:
+    def __init__(self, name, weight):
+        self.name = name
+        self.weight = weight
+
+    def __str__(self):
+        return f"{self.name} ({self.weight} kg)"
+
+
+class Box:
+    def __init__(self):
+        self.weight = 0
+
+    def add_present(self, present: Present):
+        self.weight += present.weight
+
+    def total_weight(self):
+        return self.weight
+
+
+# testing
+book = Present("ABC Book", 2)
+
+print("The name of the present:", book.name)
+print("The weight of the present:", book.weight)
+print("Present:", book)
