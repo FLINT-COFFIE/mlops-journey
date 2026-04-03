4f601d0e50612bc4453bde8efa65501b61c299cf
Flint Coffie <coffieflint91@gmail.com>
2026-04-03 08:30:12 +0000

    completed box of presents'

--- helsinki-mooc/mooc-programming-26/part09-07_box_of_presents/src/box_of_presents.py
+++ helsinki-mooc/mooc-programming-26/part09-07_box_of_presents/src/box_of_presents.py
@@ -19,9 +19,14 @@ class Box:
         return self.weight
 
 
-# testing
-book = Present("ABC Book", 2)
+if __name__ == "__main__":
+    # testing
+    book = Present("ABC Book", 2)
 
-print("The name of the present:", book.name)
-print("The weight of the present:", book.weight)
-print("Present:", book)
+    box = Box()
+    box.add_present(book)
+    print(box.total_weight())
+
+    cd = Present("Pink Floyd: Dark Side of the Moon", 1)
+    box.add_present(cd)
+    print(box.total_weight())
