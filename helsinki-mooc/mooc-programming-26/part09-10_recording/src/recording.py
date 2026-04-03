4285bb1de7e4ae0778e2705dfea18f7f6d54ce79
Flint Coffie <coffieflint91@gmail.com>
2026-04-03 19:18:31 +0000

    completed recording using getters and setters

--- helsinki-mooc/mooc-programming-26/part09-10_recording/src/recording.py
+++ helsinki-mooc/mooc-programming-26/part09-10_recording/src/recording.py
@@ -1,1 +1,27 @@
 # WRITE YOUR SOLUTION HERE:
+class Recording:
+    def __init__(self, length):
+        if length < 0:
+            raise ValueError("The amount must not be below zero")
+        else:
+            self.__length = length
+
+    # A getter method
+    @property
+    def length(self):
+        return self.__length
+
+    @length.setter
+    def length(self, length):
+        if length < 0:
+            raise ValueError("The amount must not be below zero")
+        else:
+            self.__length = length
+
+
+if __name__ == "__main__":
+    # testing
+    the_wall = Recording(43)
+    print(the_wall.length)
+    the_wall.length = 44
+    print(the_wall.length)
