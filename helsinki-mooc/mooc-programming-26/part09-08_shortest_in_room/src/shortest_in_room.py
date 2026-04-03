27e642051352b86d571343d60f7c162bce4d21f6
Flint Coffie <coffieflint91@gmail.com>
2026-04-03 11:14:08 +0000

    shortest person in the room

--- helsinki-mooc/mooc-programming-26/part09-08_shortest_in_room/src/shortest_in_room.py
+++ helsinki-mooc/mooc-programming-26/part09-08_shortest_in_room/src/shortest_in_room.py
@@ -6,3 +6,31 @@ class Person:
 
     def __str__(self):
         return self.name
+
+class Room:
+    def __init__(self, people: list):
+        self.people = people
+        
+    def add(self, person: Person):
+        self.people += person
+        
+    def is_empty(self):
+        if len(self.people) == 0:
+            return True
+        return False
+    
+    def print_contents(self):
+        for person in self.people:
+            print person
+            
+            
+#testing
+room = Room()
+print("Is the room empty?", room.is_empty())
+room.add(Person("Lea", 183))
+room.add(Person("Kenya", 172))
+room.add(Person("Ally", 166))
+room.add(Person("Nina", 162))
+room.add(Person("Dorothy", 155))
+print("Is the room empty?", room.is_empty())
+room.print_contents()
