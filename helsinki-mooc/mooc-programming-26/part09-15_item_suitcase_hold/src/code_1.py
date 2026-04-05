86b4e23b125a39a1391ba330bfe761be28e188be
Flint Coffie <coffieflint91@gmail.com>
2026-04-05 18:15:18 +0000

    part 5

--- helsinki-mooc/mooc-programming-26/part09-15_item_suitcase_hold/src/code_1.py
+++ helsinki-mooc/mooc-programming-26/part09-15_item_suitcase_hold/src/code_1.py
@@ -7,18 +7,61 @@ class Item:
         self.__name = name
         self.__weight = weight
 
+    def weight(self):
+        return self.__weight
+
+    def name(self):
+        return self.__name
+
 
 # second class
 class Suitcase:
     def __init__(self, maximum_weight):
         self.__maximum_weight = maximum_weight
-        self.__current_weight = 0
         self.__items = []
 
+    def weight(self):
+        current_weight = 0
+        for item in self.__items:
+            current_weight += item.weight()
+        return current_weight
+
     def add_item(self, item: Item):
-        if item.__weight + self.__current_weight <= 5:
-            self.__current_weight += item.__weight
+        total_weight = self.weight()
+        if item.weight() + total_weight <= self.__maximum_weight:
+            total_weight += item.weight()
             self.__items.append(item)
 
     def __str__(self):
-        return f"{len(self.__items)}"
+        if len(self.__items) == 1:
+            return f"1 item ({self.weight} kg)"
+        return f"{len(self.__items)} items ({self.weight} kg)"
+
+    def print_items(self):
+        for item in self.__items:
+            print(f"{item.name()} ({item.weight()} kg)")
+
+    def heaviest_item(self):
+        heaviest = []
+        heavy = None
+        for item in self.__items:
+            heaviest.append(item.weight())
+        max_weight = max(heaviest)
+        for item in self.__items:
+            if item.weight == max_weight:
+                heavy = item
+        return heavy
+
+
+# testing
+book = Item("ABC Book", 2)
+phone = Item("Nokia 3210", 1)
+brick = Item("Brick", 4)
+
+suitcase = Suitcase(10)
+suitcase.add_item(book)
+suitcase.add_item(phone)
+suitcase.add_item(brick)
+
+heaviest = suitcase.heaviest_item()
+print(f"The heaviest item: {heaviest}")
