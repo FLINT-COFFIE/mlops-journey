c0bd2b3657b7ce08a9a4a5671669a0c49522e8cc
Flint Coffie <coffieflint91@gmail.com>
2026-04-05 09:23:11 +0000

    started list_helper

--- helsinki-mooc/mooc-programming-26/part09-14_list_helper/src/list_helper.py
+++ helsinki-mooc/mooc-programming-26/part09-14_list_helper/src/list_helper.py
@@ -1,1 +1,25 @@
 # WRITE YOUR SOLUTION HERE:
+from collections import Counter
+
+
+class ListHelper:
+    @classmethod
+    def greatest_frequency(cls, my_list: list):
+        unique = Counter(my_list)
+        highest = unique.most_common(1)
+        return highest[0][0]
+
+    @classmethod
+    def doubles(cls, my_list: list):
+        unique = Counter(my_list)
+        doubles = []
+        for num, counts in unique.items():
+            if counts >= 2:
+                doubles.append(num)
+        return doubles
+
+
+# testing
+numbers = [1, 1, 2, 1, 3, 3, 4, 5, 5, 5, 6, 5, 5, 5]
+print(ListHelper.greatest_frequency(numbers))
+print(ListHelper.doubles(numbers))
