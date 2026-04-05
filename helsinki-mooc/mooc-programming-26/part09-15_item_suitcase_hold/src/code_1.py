28cda9150ebef8c873af5843ced03657f42bb081
Flint Coffie <coffieflint91@gmail.com>
2026-04-05 19:54:29 +0000

    completed part 9

--- helsinki-mooc/mooc-programming-26/part09-15_item_suitcase_hold/src/code_1.py
+++ helsinki-mooc/mooc-programming-26/part09-15_item_suitcase_hold/src/code_1.py
@@ -13,6 +13,9 @@ class Item:
     def name(self):
         return self.__name
 
+    def __str__(self):
+        return f"{self.__name} ({self.weight()} kg)"
+
 
 # second class
 class Suitcase:
@@ -38,10 +41,14 @@ class Suitcase:
 
     def heaviest_item(self):
         # heaviest = []
-        heaviest = None
+        if len(self.__items) == 0:
+            return None
+
+        heaviest = self.__items[0]
         # for item in self.__items:
         #    heaviest.append(item.weight())
         # max_weight = max(heaviest)
+
         for item in self.__items:
             if item.weight() > heaviest.weight():
                 heaviest = item  # f"{item.name()} ({item.weight()} kg)"
@@ -68,31 +75,34 @@ class CargoHold:
         if suitcase.weight() + self.current_weight() <= self.__maximum_weight:
             self.__cargo.append(suitcase)
 
+    def print_items(self):
+        for suitcase in self.__cargo:
+            suitcase.print_items()
+
     def __str__(self):
         if len(self.__cargo) == 1:
             return f"1 suitcase, space for {self.__maximum_weight - self.current_weight()} kg"
         return f"{len(self.__cargo)} suitcases, space for {self.__maximum_weight - self.current_weight()} kg"
 
-    def print_items(self):
-        for suitcase in self.__cargo:
-            suitcase.print_items()
 
+if __name__ == "__main__":
+    # testing
+    cargo_hold = CargoHold(1000)
+    print(cargo_hold)
 
-# testing
-book = Item("ABC Book", 2)
-phone = Item("Nokia 3210", 1)
-brick = Item("Brick", 4)
+    book = Item("ABC Book", 2)
+    phone = Item("Nokia 3210", 1)
+    brick = Item("Brick", 4)
 
-adas_suitcase = Suitcase(10)
-adas_suitcase.add_item(book)
-adas_suitcase.add_item(phone)
+    adas_suitcase = Suitcase(10)
+    adas_suitcase.add_item(book)
+    adas_suitcase.add_item(phone)
 
-peters_suitcase = Suitcase(10)
-peters_suitcase.add_item(brick)
+    peters_suitcase = Suitcase(10)
+    peters_suitcase.add_item(brick)
 
-cargo_hold = CargoHold(1000)
-cargo_hold.add_suitcase(adas_suitcase)
-cargo_hold.add_suitcase(peters_suitcase)
+    cargo_hold.add_suitcase(adas_suitcase)
+    print(cargo_hold)
 
-print("The suitcases in the cargo hold contain the following items:")
-cargo_hold.print_items()
+    cargo_hold.add_suitcase(peters_suitcase)
+    print(cargo_hold)
