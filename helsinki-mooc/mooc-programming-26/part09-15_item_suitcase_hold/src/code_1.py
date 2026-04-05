5c95bc2093039c2502ba21574a2c3ba7b237bda9
Flint Coffie <coffieflint91@gmail.com>
2026-04-05 17:32:05 +0000

    completed part 1 and 2

--- helsinki-mooc/mooc-programming-26/part09-15_item_suitcase_hold/src/code_1.py
+++ helsinki-mooc/mooc-programming-26/part09-15_item_suitcase_hold/src/code_1.py
@@ -1,5 +1,24 @@
 # Write your solution here:
+# first class
 class Item:
+    # constructor
     def __init__(self, name, weight):
+        # hidden attributes(encapsulated)
         self.__name = name
         self.__weight = weight
+
+
+# second class
+class Suitcase:
+    def __init__(self, maximum_weight):
+        self.__maximum_weight = maximum_weight
+        self.__current_weight = 0
+        self.__items = []
+
+    def add_item(self, item: Item):
+        if item.__weight + self.__current_weight <= 5:
+            self.__current_weight += item.__weight
+            self.__items.append(item)
+
+    def __str__(self):
+        return f"{len(self.__items)}"
