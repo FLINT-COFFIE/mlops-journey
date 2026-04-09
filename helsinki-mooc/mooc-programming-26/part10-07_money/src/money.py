c6b62be04e887c95b79d84ff70d2e411fc50b1ab
Flint Coffie <coffieflint91@gmail.com>
2026-04-09 20:26:02 +0000

    completed money

--- helsinki-mooc/mooc-programming-26/part10-07_money/src/money.py
+++ helsinki-mooc/mooc-programming-26/part10-07_money/src/money.py
@@ -1,46 +1,46 @@
 # TEE RATKAISUSI TÄHÄN:
 class Money:
     def __init__(self, euros: int, cents: int):
-        self.euros = euros
-        self.cents = cents
+        self._euros = euros
+        self._cents = cents
 
     def __str__(self):
-        return f"{self.euros}.{self.cents:02d} eur"
+        return f"{self._euros}.{self._cents:02d} eur"
 
     # equality between objects
     def __eq__(self, another):
-        return self.euros == another.euros and self.cents == another.cents
+        return self._euros == another._euros and self._cents == another._cents
 
     # other comparison operators
     def __lt__(self, another):
-        if self.euros < another.euros:
+        if self._euros < another._euros:
             return True
-        elif self.euros == another.euros and self.cents < another.cents:
+        elif self._euros == another._euros and self._cents < another._cents:
             return True
         return False
 
     def __gt__(self, another):
-        if self.euros > another.euros:
+        if self._euros > another._euros:
             return True
-        elif self.euros == another.euros and self.cents > another.cents:
+        elif self._euros == another._euros and self._cents > another._cents:
             return True
         return False
 
     def __ne__(self, another):
-        return self.euros != another.euros or self.cents != another.cents
+        return self._euros != another._euros or self._cents != another._cents
 
     # addition and subtraction
     def __add__(self, another):
-        new_total = (self.euros * 100 + self.cents) + (
-            another.euros * 100 + another.cents
+        new_total = (self._euros * 100 + self._cents) + (
+            another._euros * 100 + another._cents
         )
         new_euros = new_total // 100
         new_cents = new_total % 100
         return Money(new_euros, new_cents)
 
     def __sub__(self, another):
-        new_total = (self.euros * 100 + self.cents) - (
-            another.euros * 100 + another.cents
+        new_total = (self._euros * 100 + self._cents) - (
+            another._euros * 100 + another._cents
         )
         if new_total < 0:
             raise ValueError("a negative result is not allowed")
@@ -50,14 +50,16 @@ class Money:
             return Money(new_euros, new_cents)
 
 
-# if __name__ == "__main__":
-e1 = Money(4, 5)
-e2 = Money(2, 95)
+if __name__ == "__main__":
+    e1 = Money(4, 5)
+    e2 = Money(2, 95)
 
-e3 = e1 + e2
-e4 = e1 - e2
+    e3 = e1 + e2
+    e4 = e1 - e2
 
-print(e3)
-print(e4)
+    print(e3)
+    print(e4)
 
-e5 = e2 - e1
+    print(e1)
+    e1.euros = 1000
+    print(e1)
