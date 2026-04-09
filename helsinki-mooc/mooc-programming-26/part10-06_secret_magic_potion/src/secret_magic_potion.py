89dc92cdad6c43143dfa581bc3952341610a37dd
Flint Coffie <coffieflint91@gmail.com>
2026-04-09 16:19:51 +0000

    testing and debugging

--- helsinki-mooc/mooc-programming-26/part10-06_secret_magic_potion/src/secret_magic_potion.py
+++ helsinki-mooc/mooc-programming-26/part10-06_secret_magic_potion/src/secret_magic_potion.py
@@ -13,3 +13,18 @@ class MagicPotion:
             print(f"{ingredient[0]} {ingredient[1]} grams")
 
 
+# secret potion
+class SecretMagicPotion(MagicPotion):
+    def __init__(self, name: str, password: str):
+        super().__init__(name)
+        self._password = password
+
+    # first method
+    def add_ingredient(self, ingredient: str, amount: float, password: str):
+        if password == self._password:
+            super().add_ingredient(ingredient, amount)
+
+    # second method
+    def print_recipe(self, password: str):
+        if password == self._password:
+            return super().print_recipe()
