8e3565da43dad4b05611b0173990ee1ee3a0eb2a
Flint Coffie <coffieflint91@gmail.com>
2026-04-09 19:48:48 +0000

    debugging equality

--- helsinki-mooc/mooc-programming-26/part10-07_money/src/money.py
+++ helsinki-mooc/mooc-programming-26/part10-07_money/src/money.py
@@ -5,4 +5,24 @@ class Money:
         self.cents = cents
 
     def __str__(self):
-        return f"{self.euros}.{self.cents}"
+        fig = f"{self.euros}.{self.cents}"
+        fig = float(fig)
+        if self.cents < 10:
+            return f"{self.euros}.0{self.cents} eur"
+        return f"{fig:.2f} eur"
+
+    # equality between objects
+    def __eq__(self, another):
+        return self == another
+
+
+if __name__ == "__main__":
+    e1 = Money(4, 10)
+    e2 = Money(2, 5)
+    e3 = Money(4, 10)
+
+    print(e1)
+    print(e2)
+    print(e3)
+    print(e1 == e2)
+    print(e1 == e3)
