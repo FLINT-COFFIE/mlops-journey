280a684d36c0d59a8d3a41118626217ed107702c
Flint Coffie <coffieflint91@gmail.com>
2026-04-09 15:39:15 +0000

    last method left

--- helsinki-mooc/mooc-programming-26/part10-05_supergroup/src/supergroup.py
+++ helsinki-mooc/mooc-programming-26/part10-05_supergroup/src/supergroup.py
@@ -5,4 +5,30 @@ class SuperHero:
         self.superpowers = superpowers
 
     def __str__(self):
-        return f'{self.name}, superpowers: {self.superpowers}'
+        return f"{self.name}, superpowers: {self.superpowers}"
+
+
+class SuperGroup(SuperHero):
+    def __init__(self, name: str, location: str):
+        super().__init__(name)
+        self._name = super().name
+        self._location = location
+        self._members = []
+        
+    # Getters
+    
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def location(self):
+        return self.location
+
+    # Add member method
+    def add_member(self, hero: SuperHero):
+        self.members.append(hero)
+        
+    # print_group method
+    def print_group(self):
+        
