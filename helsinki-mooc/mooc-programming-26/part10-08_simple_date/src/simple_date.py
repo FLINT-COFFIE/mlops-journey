709c938f6ca8d0f48f5abe9509f3f69887b3bbaa
Flint Coffie <coffieflint91@gmail.com>
2026-04-10 13:31:24 +0000

    completed less than

--- helsinki-mooc/mooc-programming-26/part10-08_simple_date/src/simple_date.py
+++ helsinki-mooc/mooc-programming-26/part10-08_simple_date/src/simple_date.py
@@ -1,2 +1,21 @@
 # WRITE YOUR SOLUTION HERE:
 # simple date comparison
+class SimpleDate:
+    def __init__(self, day: int, month: int, year: int):
+        self.day = day
+        self.month = month
+        self.year = year
+
+    def __lt__(self, another: "SimpleDate"):
+        if self.year > another.year:
+            return True
+        elif self.year == another.year and self.month > another.month:
+            return True
+        elif (
+            self.year == another.year
+            and self.month > another.month
+            and self.day > another.day
+        ):
+            return True
+        else:
+            return True
