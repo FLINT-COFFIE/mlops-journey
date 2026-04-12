e30d5bfdd3d8b13c7403d35fddd9430838b238c6
Flint Coffie <coffieflint91@gmail.com>
2026-04-12 18:55:21 +0000

    completed part 1

--- helsinki-mooc/mooc-programming-26/part11-07_lottery_numbers/src/lottery_numbers.py
+++ helsinki-mooc/mooc-programming-26/part11-07_lottery_numbers/src/lottery_numbers.py
@@ -1,1 +1,15 @@
 # WRITE YOUR SOLUTION HERE:
+class LotteryNumbers:
+    def __init__(self, week: int, numbers: list):
+        self.week = week
+        self.numbers = numbers
+
+    def number_of_hits(self, numbers: list):
+        return len([number for number in numbers if number in self.numbers])
+
+
+# testing
+week5 = LotteryNumbers(5, [1, 2, 3, 4, 5, 6, 7])
+my_numbers = [1, 4, 7, 11, 13, 19, 24]
+
+print(week5.number_of_hits(my_numbers))
