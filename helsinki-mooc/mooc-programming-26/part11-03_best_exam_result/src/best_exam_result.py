5326bc12b20ca39e218bcf2cfdc6e400a6220441
Flint Coffie <coffieflint91@gmail.com>
2026-04-12 15:13:25 +0000

    defined best results

--- helsinki-mooc/mooc-programming-26/part11-03_best_exam_result/src/best_exam_result.py
+++ helsinki-mooc/mooc-programming-26/part11-03_best_exam_result/src/best_exam_result.py
@@ -6,6 +6,11 @@ class ExamResult:
         self.grade2 = grade2
         self.grade3 = grade3
 
+    def best_results(results: list):
+        return [max(num) for num in list(results[1:])]
+
     def __str__(self):
-        return (f'Name:{self.name}, grade1: {self.grade1}' +
-            f', grade2: {self.grade2}, grade3: {self.grade3}')
+        return (
+            f"Name:{self.name}, grade1: {self.grade1}"
+            + f", grade2: {self.grade2}, grade3: {self.grade3}"
+        )
