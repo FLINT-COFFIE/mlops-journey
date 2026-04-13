5ec45e373d8538e951b10c795baaeaaf2a0ea151
Flint Coffie <coffieflint91@gmail.com>
2026-04-13 17:10:10 +0000

    completed recursive sum

--- helsinki-mooc/mooc-programming-26/part11-14_recursive_sum/src/recursive_sum.py
+++ helsinki-mooc/mooc-programming-26/part11-14_recursive_sum/src/recursive_sum.py
@@ -5,3 +5,12 @@ def recursive_sum(number: int):
         return number
     else:
         return number + recursive_sum(number - 1)
+    
+
+if __name__ == "__main__":    
+    #testing
+    result = recursive_sum(3)
+    print(result)
+
+    print(recursive_sum(5))
+    print(recursive_sum(10))
