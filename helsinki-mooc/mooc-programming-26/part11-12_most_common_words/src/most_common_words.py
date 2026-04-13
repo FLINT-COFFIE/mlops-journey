599a9b4ed7128dfe3d886bd5f226dac5bb44a2cf
Flint Coffie <coffieflint91@gmail.com>
2026-04-13 11:14:23 +0000

    completed most common words

--- helsinki-mooc/mooc-programming-26/part11-12_most_common_words/src/most_common_words.py
+++ helsinki-mooc/mooc-programming-26/part11-12_most_common_words/src/most_common_words.py
@@ -1,5 +1,7 @@
 # WRITE YOUR SOLUTION HERE:
 def most_common_words(filename: str, lower_limit: int):
     with open(filename) as words:
+        words = words.read()
+        words = words.replace(".", "").replace(",","")
         words = words.split()
-        return{word: word.count() for word in words if word.count >= lower_limit}
+        return{word : words.count(word) for word in words if words.count(word) >= lower_limit}
