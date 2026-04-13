e027e74ec3467bdcdfe7517d61d1b3cf1d312513
Flint Coffie <coffieflint91@gmail.com>
2026-04-13 10:51:49 +0000

    completed lengths of strings

--- helsinki-mooc/mooc-programming-26/part11-11_lengths_of_strings/src/lengths_of_strings.py
+++ helsinki-mooc/mooc-programming-26/part11-11_lengths_of_strings/src/lengths_of_strings.py
@@ -2,9 +2,9 @@
 def lengths(strings: list):
     return {word : len(word) for word in strings}
 
+if __name__ == "__main__":
+    #testing
+    word_list = ["once", "upon" , "a", "time", "in"]
 
-#testing
-word_list = ["once", "upon" , "a", "time", "in"]
-
-word_lengths = lengths(word_list)
-print(word_lengths)
+    word_lengths = lengths(word_list)
+    print(word_lengths)
