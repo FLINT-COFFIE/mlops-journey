03a32504b528ac6f16f4074c79fd42d64907c252
Flint Coffie <coffieflint91@gmail.com>
2026-04-13 11:01:11 +0000

    defined most common words

--- helsinki-mooc/mooc-programming-26/part11-12_most_common_words/src/most_common_words.py
+++ helsinki-mooc/mooc-programming-26/part11-12_most_common_words/src/most_common_words.py
@@ -1,1 +1,5 @@
 # WRITE YOUR SOLUTION HERE:
+def most_common_words(filename: str, lower_limit: int):
+    with open(filename) as words:
+        words = words.split()
+        return{word: word.count() for word in words if word.count >= lower_limit}
