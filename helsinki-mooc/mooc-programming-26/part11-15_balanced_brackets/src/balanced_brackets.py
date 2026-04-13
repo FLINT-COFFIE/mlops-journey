f557030108505ac9779b48facb1840cc9c60cda1
Flint Coffie <coffieflint91@gmail.com>
2026-04-13 19:19:54 +0000

    completed balanced brackets

--- helsinki-mooc/mooc-programming-26/part11-15_balanced_brackets/src/balanced_brackets.py
+++ helsinki-mooc/mooc-programming-26/part11-15_balanced_brackets/src/balanced_brackets.py
@@ -1,6 +1,6 @@
 
 def balanced_brackets(my_string: str):
-    filtered = "".join([bracket for bracket in my_string if bracket in "()[]"])
+    filtered = "".join([bracket for bracket in my_string if bracket in ["(",")","[","]"]])
     
     def checks(char: str):
         if len(char) == 0:
@@ -9,9 +9,9 @@ def balanced_brackets(my_string: str):
         round = char[0] == "(" and char [-1] == ")"
         square = char[0] == "[" and char [-1] == "]"
         
-        if not round or square:
+        if not (round or square):
             return False
         
-        return checks(s[1:-1])
+        return checks(char[1:-1])
     return checks(filtered)
         
