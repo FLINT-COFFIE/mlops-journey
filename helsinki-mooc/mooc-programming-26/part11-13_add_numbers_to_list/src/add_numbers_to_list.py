c7baf1e06f337cf4677eb5eee8832e1f30a1499d
Flint Coffie <coffieflint91@gmail.com>
2026-04-13 13:16:41 +0000

    completed add numbers to a list

--- helsinki-mooc/mooc-programming-26/part11-13_add_numbers_to_list/src/add_numbers_to_list.py
+++ helsinki-mooc/mooc-programming-26/part11-13_add_numbers_to_list/src/add_numbers_to_list.py
@@ -2,4 +2,11 @@
 def add_numbers_to_list(numbers: list):
     if len(numbers) % 5 != 0:
         numbers.append(numbers[-1] + 1)
-    add_numbers_to_list(numbers) 
+        add_numbers_to_list(numbers) 
+    
+
+if __name__ == "__main__":
+    #testing
+    numbers = [1,3,4,5,10,11]
+    add_numbers_to_list(numbers)
+    print(numbers)
