2436b7271e18e0e896acf6284c8016cdc51e3f42
Flint Coffie <coffieflint91@gmail.com>
2026-04-15 21:54:48 +0000

    defined function and helper function:

--- helsinki-mooc/mooc-programming-26/part12-01_remaining_stock/src/remaining_stock.py
+++ helsinki-mooc/mooc-programming-26/part12-01_remaining_stock/src/remaining_stock.py
@@ -1,1 +1,7 @@
 # Write your solution here:
+def sort_by_remaining_stock(items: list):
+    #helper function
+    def stock():
+        return items[2]
+    
+    return sorted(items, key=sort_by_remaining_stock())
