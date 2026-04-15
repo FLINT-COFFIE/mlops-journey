40b2267ac366e1025ebab597dc8d447a9f5fc50e
Flint Coffie <coffieflint91@gmail.com>
2026-04-15 21:57:55 +0000

    completed remaining stock

--- helsinki-mooc/mooc-programming-26/part12-01_remaining_stock/src/remaining_stock.py
+++ helsinki-mooc/mooc-programming-26/part12-01_remaining_stock/src/remaining_stock.py
@@ -1,7 +1,15 @@
 # Write your solution here:
 def sort_by_remaining_stock(items: list):
     #helper function
-    def stock():
+    def stock(items):
         return items[2]
     
-    return sorted(items, key=sort_by_remaining_stock())
+    return sorted(items, key=stock)
+
+
+if __name__ == "__main__":
+    #testing
+    products = [("banana", 5.95, 12), ("apple", 3.95, 3), ("orange", 4.50, 2), ("watermelon", 4.95, 22)]
+
+    for product in sort_by_remaining_stock(products):
+        print(f"{product[0]} {product[2]} pcs")
