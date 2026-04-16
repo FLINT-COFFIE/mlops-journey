e12874fc9a930d908cda1631b3ec03c2808eacc4
Flint Coffie <coffieflint91@gmail.com>
2026-04-16 06:47:47 +0000

    defined sort by seasons function

--- helsinki-mooc/mooc-programming-26/part12-02_seasons/src/seasons.py
+++ helsinki-mooc/mooc-programming-26/part12-02_seasons/src/seasons.py
@@ -1,1 +1,6 @@
 # Write your solution here:
+def sort_by_seasons(items: list):
+    def seasons(items):
+        return [items[i]["seasons"] for i in items]
+    return sorted(items, key=seasons(items))
+
