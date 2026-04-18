b3521b1f3fbd608cf928def4a185a7deea1e8091
Flint Coffie <coffieflint91@gmail.com>
2026-04-18 22:21:25 +0000

    completed prime numbers

--- helsinki-mooc/mooc-programming-26/part12-09_prime_numbers/src/prime_numbers.py
+++ helsinki-mooc/mooc-programming-26/part12-09_prime_numbers/src/prime_numbers.py
@@ -1,9 +1,22 @@
 # Write your solution here
 def prime_numbers():
-    x = 2
-    for i in range(2, x):
-        if x / i != 1:
-            continue
-    else:
-        yield x
-    x += 1
+    try:
+        x = 2
+        while True:
+            for i in range(2, x):
+                if x % i == 0:
+                    break
+                   
+            else:
+                yield x
+            x += 1
+    
+    except StopIteration:
+        print("ran out of numbers")
+        
+
+if __name__ == "__main__":
+    #testing
+    numbers = prime_numbers()
+    for i in range(8):
+        print(next(numbers))
