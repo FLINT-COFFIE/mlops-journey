12b9ffcfa28818e82790eeb03ec5ceae4c493c8d
Flint Coffie <coffieflint91@gmail.com>
2026-04-18 21:58:01 +0000

    completed even numbers

--- helsinki-mooc/mooc-programming-26/part12-08_even_numbers/src/even_numbers.py
+++ helsinki-mooc/mooc-programming-26/part12-08_even_numbers/src/even_numbers.py
@@ -1,7 +1,16 @@
 # Write your solution here
 def even_numbers(beginning: int, maximum: int):
     number = beginning
-    while number <= maximum and number % 2 == 0:
-        yield number
-        number += 1
-    
+    while number <= maximum:
+        if number % 2 == 0:
+            yield number
+            number += 2
+        else:
+            number += 1
+        
+
+if __name__ == "__main__":
+    numbers = even_numbers(2, 10)
+    for number in numbers:
+        print(number)
+        
