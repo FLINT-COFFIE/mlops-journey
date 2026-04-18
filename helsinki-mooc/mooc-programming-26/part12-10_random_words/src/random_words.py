1aab595480efeefcc6a5e745569af7ed22ce1315
Flint Coffie <coffieflint91@gmail.com>
2026-04-18 22:50:25 +0000

    completed random words

--- helsinki-mooc/mooc-programming-26/part12-10_random_words/src/random_words.py
+++ helsinki-mooc/mooc-programming-26/part12-10_random_words/src/random_words.py
@@ -1,3 +1,9 @@
 # Write your solution here:
 def word_generator(characters: str, length: int, amount: int):
-    return (characters[i : length] for i in range(amount))
+    return (characters[i : i + 3] for i in range(amount))
+
+
+if __name__ == "__main__":
+    wordgen = word_generator("abcdefg", 3, 5)
+    for word in wordgen:
+        print(word)
