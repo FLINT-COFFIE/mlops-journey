0e1a055bcff33f5bc574352adfbcb892c2981453
Flint Coffie <coffieflint91@gmail.com>
2026-04-19 17:33:18 +0000

    completed credits

--- helsinki-mooc/mooc-programming-26/part12-13_credits/src/credits.py
+++ helsinki-mooc/mooc-programming-26/part12-13_credits/src/credits.py
@@ -16,11 +16,20 @@ def sum_of_all_credits(attempts: list):
 
 def sum_of_passed_credits(attempts: list):
     passed = list(filter(lambda attempt: attempt.grade >= 1, attempts))
-    return sum_of_all_credits(passed)
+    sum_credits = reduce(lambda credits, item: credits + item.credits, passed, 0)
+    return sum_credits
+    
+    
+def average(attempts: list):
+    passed = list(filter(lambda attempt: attempt.grade >= 1, attempts))
+    grades = reduce(lambda credits, item: credits + item.grade, attempts, 0)
+    return grades / len(passed)
 
-#testing
-s1 = CourseAttempt("Introduction to Programming", 5, 5)
-s2 = CourseAttempt("Advanced Course in Programming", 0, 4)
-s3 = CourseAttempt("Data Structures and Algorithms", 3, 10)
-credit_sum = sum_of_passed_credits([s1, s2, s3])
-print(credit_sum)
+
+if __name__ == "__main__":
+    #testing
+    s1 = CourseAttempt("Introduction to Programming", 5, 5)
+    s2 = CourseAttempt("Advanced Course in Programming", 0, 4)
+    s3 = CourseAttempt("Data Structures and Algorithms", 3, 10)
+    ag = average([s1, s2, s3])
+    print(ag)
