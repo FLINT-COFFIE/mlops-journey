f70c1f3d903ac18938f10f294c9aaa267a48bb7c
Flint Coffie <coffieflint91@gmail.com>
2026-04-19 20:56:23 +0000

    completed pated part 1 of regular expression

--- helsinki-mooc/mooc-programming-26/part12-14_regular_expressions/src/regular_expressions.py
+++ helsinki-mooc/mooc-programming-26/part12-14_regular_expressions/src/regular_expressions.py
@@ -1,2 +1,14 @@
 # Write your solution here
 import re
+
+def is_dotw(my_string: str):
+    expression = "Mon|Tue|Wed|Thu|Fri|Sat|Sun"
+    if re.search(expression, my_string):
+        return True
+    return False
+
+
+#testing
+print(is_dotw("Mon"))
+print(is_dotw("Fri"))
+print(is_dotw("Tui"))
