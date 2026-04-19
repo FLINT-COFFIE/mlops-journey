ae550ddce436bae51f88874d42c74f0f8b32d9e7
Flint Coffie <coffieflint91@gmail.com>
2026-04-19 17:06:01 +0000

    completed part 1

--- helsinki-mooc/mooc-programming-26/part12-13_credits/src/credits.py
+++ helsinki-mooc/mooc-programming-26/part12-13_credits/src/credits.py
@@ -10,3 +10,14 @@ class CourseAttempt:
         return f"{self.course_name} ({self.credits} cr) grade {self.grade}"
 
 # Write your solution
+def sum_of_all_credits(attempts: list):
+    sum_credits = reduce(lambda credits, item: credits + item.credits, attempts, 0)
+    return sum_credits
+
+
+#testing
+s1 = CourseAttempt("Introduction to Programming", 5, 5)
+s2 = CourseAttempt("Advanced Course in Programming", 4, 5)
+s3 = CourseAttempt("Data Structures and Algorithms", 3, 10)
+credit_sum = sum_of_all_credits([s1, s2, s3])
+print(credit_sum)
