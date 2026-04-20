7e017f1d8b7abbea36e6797b5c32265b19dfc691
Flint Coffie <coffieflint91@gmail.com>
2026-04-20 22:31:22 +0000

    moved from top right to the bottom right and stopped

--- helsinki-mooc/mooc-programming-26/part13-05_vertical_movement/src/main.py
+++ helsinki-mooc/mooc-programming-26/part13-05_vertical_movement/src/main.py
@@ -1,1 +1,35 @@
-# WRITE YOUR SOLUTION HERE:
+#write your solution here
+import pygame
+
+pygame.init()
+window = pygame.display.set_mode((640, 480))
+
+robot = pygame.image.load("robot.png")
+
+width = robot.get_width()
+height = robot.get_height()
+
+max_x = 640 - width
+max_y = 480 - height
+
+x = max_x
+y = 0
+
+velocity = 1
+
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+
+    window.fill((0, 0, 0))
+    window.blit(robot, (x, y))
+    pygame.display.flip()
+    
+    if y < max_y and x == max_x:
+        y += velocity
+        
+
+    clock.tick(60)
