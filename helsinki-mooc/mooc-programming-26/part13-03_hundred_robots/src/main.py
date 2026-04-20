009419499cf7803f33fe5be1c4388d252f0bb510
Flint Coffie <coffieflint91@gmail.com>
2026-04-20 18:27:37 +0000

    completed 100 robots

--- helsinki-mooc/mooc-programming-26/part13-03_hundred_robots/src/main.py
+++ helsinki-mooc/mooc-programming-26/part13-03_hundred_robots/src/main.py
@@ -1,1 +1,42 @@
 # WRITE YOUR SOLUTION HERE:
+
+# The exercises in this part of the course have no automated tests, as the results as visually verified.
+# The tests grant points automatically as you submit your solution to the server, no matter what your implementation.
+# Only submit your solution when you are ready, and your solution matches the exercise description.
+# The exercises may not have automatic tests, but the course staff will still see your solution.
+# If your solution clearly does not match the exercise description, you may lose the points granted for the exercises in this part.
+
+# WRITE YOUR SOLUTION HERE:
+
+import pygame
+
+pygame.init()
+window = pygame.display.set_mode((640, 480))
+
+robot = pygame.image.load("robot.png")
+width = robot.get_width()
+height = robot.get_height()
+
+window.fill((0, 0, 0))
+
+
+def row():
+    y = 100
+    x = 50
+    count = 1
+    while count <= 10:
+        for bot in range(10):
+            window.blit(robot, (x, y))
+            #decreased width to make it look more like the picture
+            x += width - 10
+        x = 50 + count * 9
+        y = 100 + count * 20
+        count += 1
+    
+row()
+pygame.display.flip()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
