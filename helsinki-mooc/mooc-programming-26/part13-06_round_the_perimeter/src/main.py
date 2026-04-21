a2ee08281352faa6d83a5c85ca6361b54dce895c
Flint Coffie <coffieflint91@gmail.com>
2026-04-21 11:21:41 +0000

    removed the comments

--- helsinki-mooc/mooc-programming-26/part13-06_round_the_perimeter/src/main.py
+++ helsinki-mooc/mooc-programming-26/part13-06_round_the_perimeter/src/main.py
@@ -8,11 +8,9 @@ robot = pygame.image.load("robot.png")
 width = robot.get_width()
 height = robot.get_height()
 
-# Pre-calculate the furthest the robot can go
 max_x = 640 - width
 max_y = 480 - height
 
-# Start position: Top-Right
 x = max_x
 y = 0
 
@@ -28,19 +26,18 @@ while True:
     window.blit(robot, (x, y))
     pygame.display.flip()
     
-    # 1. TOP EDGE: Move right if there is room
+   
     if y <= 0 and x < max_x:
         x += velocity
         
-    # 2. RIGHT EDGE: Move down if at the right wall
+    
     elif x >= max_x and y < max_y:
         y += velocity
         
-    # 3. BOTTOM EDGE: Move left if at the floor
+    
     elif y >= max_y and x > 0:
         x -= velocity
         
-    # 4. LEFT EDGE: Move up if at the left wall
     elif x <= 0 and y > 0:
         y -= velocity
     
