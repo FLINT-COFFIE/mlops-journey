1690f89f6dc896f1947de7104f80eb35efadc6d7
Flint Coffie <coffieflint91@gmail.com>
2026-04-21 10:31:52 +0000

    completed the two robots

--- helsinki-mooc/mooc-programming-26/part13-06_round_the_perimeter/src/main.py
+++ helsinki-mooc/mooc-programming-26/part13-06_round_the_perimeter/src/main.py
@@ -1,1 +1,47 @@
-# # WRITE YOUR SOLUTION HERE:
+import pygame
+
+pygame.init()
+window = pygame.display.set_mode((640, 480))
+
+robot = pygame.image.load("robot.png")
+
+width = robot.get_width()
+height = robot.get_height()
+
+# Pre-calculate the furthest the robot can go
+max_x = 640 - width
+max_y = 480 - height
+
+# Start position: Top-Right
+x = max_x
+y = 0
+
+velocity = 1
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+
+    window.fill((0, 0, 0))
+    window.blit(robot, (x, y))
+    pygame.display.flip()
+    
+    # 1. TOP EDGE: Move right if there is room
+    if y <= 0 and x < max_x:
+        x += velocity
+        
+    # 2. RIGHT EDGE: Move down if at the right wall
+    elif x >= max_x and y < max_y:
+        y += velocity
+        
+    # 3. BOTTOM EDGE: Move left if at the floor
+    elif y >= max_y and x > 0:
+        x -= velocity
+        
+    # 4. LEFT EDGE: Move up if at the left wall
+    elif x <= 0 and y > 0:
+        y -= velocity
+    
+    clock.tick(60)
--- helsinki-mooc/mooc-programming-26/part13-07_two_robots/src/main.py
+++ helsinki-mooc/mooc-programming-26/part13-07_two_robots/src/main.py
@@ -1,1 +1,42 @@
-# WRITE YOUR SOLUTION HERE:
+# # WRITE YOUR SOLUTION HERE:
+import pygame
+
+pygame.init()
+window = pygame.display.set_mode((640, 480))
+
+robot = pygame.image.load("robot.png")
+
+x = 0
+y = 50
+
+x1 = 0
+y1 = 150
+velocity = 1
+velocity1 = 3
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+
+    window.fill((0, 0, 0))
+    window.blit(robot, (x, y))
+    
+    window.blit(robot, (x1, y1))
+    pygame.display.flip()
+    
+    x += velocity
+    x1 += velocity1
+    
+    if velocity > 0 and x+robot.get_width() >= 640:
+        velocity = -velocity
+    if velocity < 0 and x <= 0:
+        velocity = -velocity
+        
+    if velocity1 > 0 and x1+robot.get_width() >= 640:
+        velocity1 = -velocity1
+    if velocity1 < 0 and x1 <= 0:
+        velocity1 = -velocity1
+
+    clock.tick(60)
