d5ba8e33e33b321e2cb826406f810bf6e3e1d967
Flint Coffie <coffieflint91@gmail.com>
2026-04-21 13:12:17 +0000

    completed bouncing ball

--- helsinki-mooc/mooc-programming-26/part13-09_bouncing_ball/src/main.py
+++ helsinki-mooc/mooc-programming-26/part13-09_bouncing_ball/src/main.py
@@ -1,1 +1,43 @@
 # WRITE YOUR SOLUTION HERE:
+import pygame
+
+pygame.init()
+window = pygame.display.set_mode((640, 480))
+
+ball = pygame.image.load("ball.png")
+
+width = ball.get_width()
+height = ball.get_height()
+
+x = 320
+y = 240
+
+vx = 2
+vy = 2
+
+
+
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+            
+
+    x += vx
+    y += vy
+    
+    if x <= 0 or x >= 640 - width:
+        vx = -vx
+        
+    if y <= 0 or y >= 480 - height:
+        vy = -vy
+        
+    window.fill((0, 0, 0))
+        
+    window.blit(ball, (x, y))
+
+    pygame.display.flip()
+
+    clock.tick(60)
