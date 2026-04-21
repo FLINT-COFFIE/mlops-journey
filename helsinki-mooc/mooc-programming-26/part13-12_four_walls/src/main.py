6b6a7c089e4e732e92da6e15fe7d7fa2f4d79ff2
Flint Coffie <coffieflint91@gmail.com>
2026-04-21 14:50:01 +0000

    completed four walls

--- helsinki-mooc/mooc-programming-26/part13-12_four_walls/src/main.py
+++ helsinki-mooc/mooc-programming-26/part13-12_four_walls/src/main.py
@@ -1,1 +1,61 @@
 # WRITE YOUR SOLUTION HERE:
+import pygame
+
+pygame.init()
+window = pygame.display.set_mode((640, 480))
+
+robot = pygame.image.load("robot.png")
+x = 0
+y = 480-robot.get_height()
+
+max_y = 480-robot.get_height()
+max_x = 640-robot.get_width()
+
+to_right = False
+to_left = False
+up = False
+down = False
+
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                to_left = True
+            if event.key == pygame.K_RIGHT:
+                to_right = True
+            if event.key == pygame.K_UP:
+                up = True
+            if event.key == pygame.K_DOWN:
+                down = True
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                to_left = False
+            if event.key == pygame.K_RIGHT:
+                to_right = False
+            if event.key == pygame.K_UP:
+                up = False
+            if event.key == pygame.K_DOWN:
+                down = False
+
+        if event.type == pygame.QUIT:
+            exit()
+
+    if to_right and x < max_x:
+        x += 2
+        
+    if to_left and x > 0:
+        x -= 2
+        
+    if up and y > 0:
+        y -= 2
+    if down and y < max_y:
+        y += 2
+
+    window.fill((0, 0, 0))
+    window.blit(robot, (x, y))
+    pygame.display.flip()
+
+    clock.tick(60)
