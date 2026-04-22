815f42a64ee15c0a0ea381494a1c4f0cf8cbcf44
Flint Coffie <coffieflint91@gmail.com>
2026-04-22 11:00:08 +0000

    refined valid anagram submission

--- neetcode_150/arrays_and_hashing/contains_duplicate.py
+++ neetcode_150/arrays_and_hashing/contains_duplicate.py
@@ -1,8 +1,3 @@
-# Time complexity: O(n)
-
-# Space complexity: O(n)
-
-
 class Solution:
     def hasDuplicate(self, nums: list[int]) -> bool:
         unique = list(set(nums))
--- neetcode_150/arrays_and_hashing/valid_anagram.py
+++ neetcode_150/arrays_and_hashing/valid_anagram.py
@@ -1,20 +1,3 @@
-# Time complexity: O(nlog⁡n)
-# Space complexity: O(n)
-# ===========================
-
-# Current
-# Time complexity: O(n)
-# Space complexity: O(n)
-
-## reducing time complexity to O(n)
-from collections import Counter
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        #if sorted(s) == sorted(t) and len(s) == len(t):
-        if len(s) != len(t): 
-            return False
         return Counter(s) == Counter(t)
-    
-  
-    
-
