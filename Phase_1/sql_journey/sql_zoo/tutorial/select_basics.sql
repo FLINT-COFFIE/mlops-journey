f3e374bcc9e8f6918f5604ea3a7394e0b5a9f942
Flint Coffie <coffieflint91@gmail.com>
2026-04-26 20:56:34 +0000

    completed Q3

--- Phase_1/sql_journey/sql_zoo/tutorial/select_basics.sql
+++ Phase_1/sql_journey/sql_zoo/tutorial/select_basics.sql
@@ -52,4 +52,7 @@ SELECT name FROM world WHERE name LIKE '____'
 SELECT name FROM world WHERE name in country
 
 --Q2 Find the country where the name is the capital city.
-SELECT name FROM world WHERE capital like name
+SELECT name FROM world WHERE concat(name, ' City') LIKE capital
+
+--Q3 Find the capital and the name where the capital includes the name of the country.
+SELECT capital, name from world WHERE capital like concat('%', name, '%') 
