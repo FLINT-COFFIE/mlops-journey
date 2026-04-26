9c12d43ec9d983786f339c0fd863391581659dc8
Flint Coffie <coffieflint91@gmail.com>
2026-04-26 13:50:26 +0000

    started sql basics

---
+++ Phase_1/sql_journey/sql_zoo/tutorial/select_basics.sql
@@ -0,0 +1,7 @@
+-- ############################################
+-- TOPIC: SELECT FROM WORLD TABLE OF COUNTRIES
+-- ############################################
+
+-- Q1: Show the population of Germany
+SELECT population FROM world WHERE name = 'Germany';
+
