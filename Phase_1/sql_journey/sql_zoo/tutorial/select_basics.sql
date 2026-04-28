6161bc418b7941a2f2d8eaacbf607201484fc60e
Flint Coffie <coffieflint91@gmail.com>
2026-04-28 15:35:04 +0000

    completed Q10

--- Phase_1/sql_journey/sql_zoo/tutorial/select_basics.sql
+++ Phase_1/sql_journey/sql_zoo/tutorial/select_basics.sql
@@ -97,3 +97,8 @@ SELECT name, population, area FROM world WHERE area > 3000000   OR population > 
 
 --Q8 Exclusive OR (XOR). Show the countries that are big by area (more than 3 million) or big by population (more than 250 million) but not both. Show name, population and area.
 SELECT name, population, area FROM world WHERE area > 3000000   XOR population > 250000000
+
+--Q9 Show the name and population in millions and the GDP in billions for the countries of the continent 'South America'.
+SELECT name, ROUND(population/1000000, 2), ROUND(GDP/1000000000, 2) FROM world WHERE continent LIKE "South America"
+
+--Q10 Show the name and per-capita GDP for those countries with a GDP of at least one trillion (1000000000000; that is 12 zeros). Round this value to the nearest 1000.
