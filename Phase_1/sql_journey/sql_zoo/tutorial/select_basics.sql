d3697896c6b10556c929c6ff84dbe61fe91c144e
Flint Coffie <coffieflint91@gmail.com>
2026-04-28 17:06:56 +0000

    completed part 3

--- Phase_1/sql_journey/sql_zoo/tutorial/select_basics.sql
+++ Phase_1/sql_journey/sql_zoo/tutorial/select_basics.sql
@@ -129,4 +129,7 @@ SELECT yr, subject, winner FROM nobel WHERE yr = 1950
 --Q2 Show who won the 1962 prize for literature.
 SELECT winner FROM nobel WHERE yr = 1962 AND subject = 'LITERATURE'
 
---Q3 
+--Q3 Show the year and subject that won 'Albert Einstein' his prize.
+SELECT yr, subject FROM nobel WHERE winner = "Albert Einstein"
+
+--Q4 
