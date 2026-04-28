81e972512f9f96ed4ecb5d01292ede88e41af113
Flint Coffie <coffieflint91@gmail.com>
2026-04-28 16:23:54 +0000

    completed Q13

--- Phase_1/sql_journey/sql_zoo/tutorial/select_basics.sql
+++ Phase_1/sql_journey/sql_zoo/tutorial/select_basics.sql
@@ -109,3 +109,12 @@ SELECT name, capital FROM world WHERE LENGTH(name) = LENGTH(capital)
 
 --Q12 Show the name and the capital where the first letters of each match. Don't include countries where the name and the capital are the same word.
 SELECT name, capital FROM world WHERE LEFT(name,1) = LEFT(capital,1) AND name <> capital
+
+--Q13 Find the country that has all the vowels and no spaces in its name.
+SELECT name FROM world WHERE 
+name LIKE "%a%" AND
+name LIKE "%e%" AND
+name LIKE "%i%" AND
+name LIKE "%o%" AND
+name LIKE "%u%" AND
+name NOT LIKE "% %"
