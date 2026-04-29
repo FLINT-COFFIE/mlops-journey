0981fcdb176dff06b37a09a806072c9ac79f8de6
Flint Coffie <coffieflint91@gmail.com>
2026-04-29 20:30:14 +0000

    completed select within select

--- Phase_1/sql_journey/sql_zoo/tutorial/select_within_select.sql
+++ Phase_1/sql_journey/sql_zoo/tutorial/select_within_select.sql
@@ -19,3 +19,12 @@ SELECT name FROM world WHERE gdp > ALL (SELECT gdp FROM world WHERE gdp > 0 and 
 
 --Q7 Find the largest country (by area) in each continent, show the continent, the name and the area:
 SELECT continent, name, area FROM world x WHERE area >= ALL (SELECT area FROM world y WHERE y.continent = x.continent AND population>0)
+
+--Q8 List each continent and the name of the country that comes first alphabetically.
+SELECT continent, name FROM world x WHERE name <= ALL (SELECT name FROM world y WHERE x.continent = y.continent)
+
+--Q9 Find the continents where all countries have a population <= 25000000. Then find the names of the countries associated with these continents. Show name, continent and population.
+SELECT name, continent, population FROM world WHERE 25000000 > ALL (SELECT population FROM world)
+
+--Q10 Some countries have populations more than three times that of all of their neighbours (in the same continent). Give the countries and continents.
+SELECT name, continent FROM world x WHERE 1/3 * x.population > ALL (SELECT population FROM world y WHERE x.continent = y.continent AND y.name  <> x.name AND population > 0)
