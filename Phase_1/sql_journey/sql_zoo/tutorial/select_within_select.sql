345588ec19c23165b5fe6eb2cd115ac58d1423ed
Flint Coffie <coffieflint91@gmail.com>
2026-04-29 16:50:56 +0000

    completed Q3-4

--- Phase_1/sql_journey/sql_zoo/tutorial/select_within_select.sql
+++ Phase_1/sql_journey/sql_zoo/tutorial/select_within_select.sql
@@ -8,4 +8,5 @@ SELECT name FROM world WHERE continent = "Europe" AND gdp/population > (SELECT g
 --Q3 List the name and continent of countries in the continents containing either Argentina or Australia. Order by name of the country.
 SELECT name, continent FROM world WHERE continent IN (SELECT continent FROM world WHERE name IN ("Argentina", "Australia")) ORDER BY name
 
---Q4 
+--Q4 Which country has a population that is more than United Kingdom but less than Germany? Show the name and the population.
+SELECT name, population FROM world WHERE population > (SELECT population FROM world WHERE name = "United Kingdom") AND population < (SELECT population FROM world WHERE name = "Germany")
