646002bf37a3dd4e34c4c96a9a7e2aa524c03a7b
Flint Coffie <coffieflint91@gmail.com>
2026-04-30 17:24:23 +0000

    completed sql basic then intermediate

--- Phase_1/sql_journey/sql_zoo/tutorial/null.sql
+++ Phase_1/sql_journey/sql_zoo/tutorial/null.sql
@@ -3,3 +3,28 @@ SELECT name
 FROM teacher
 WHERE dept IS NULL
 
+-- Inner Join example
+SELECT teacher.name, dept.name
+ FROM teacher INNER JOIN dept
+           ON (teacher.dept=dept.id)
+
+-- Use a different JOIN so that all teachers are listed.
+SELECT teacher.name, dept.name
+ FROM teacher LEFT JOIN dept
+           ON (teacher.dept=dept.id)
+
+-- Use a different JOIN so that all departments are listed.
+SELECT teacher.name, dept.name
+ FROM teacher RIGHT JOIN dept
+           ON (teacher.dept=dept.id)
+
+
+----- Using the COALESCE function --------
+
+-- Show teacher name and mobile number or '07986 444 2266'
+SELECT name, COALESCE(mobile, '07986 444 2266')
+FROM teacher 
+
+-- Use the COALESCE function and a LEFT JOIN to print the teacher name and department name. Use the string 'None' where there is no department.
+SELECT teacher.name, COALESCE (dept.name, 'None') AS dept
+FROM teacher LEFT JOIN dept on dept.id = teacher.dept
